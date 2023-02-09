b388499b8fbab17d37940f72f1fc9e9523381b2f
김주연 <jyeon3930@naver.com>
2023-02-09 16:02:48 +0900

    [FE] feat: 마이페이지-친구 임시 라우트 추가

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -3,11 +3,15 @@ import { Helmet } from "react-helmet-async";
 import { Route, Routes, useLocation } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import "./App.css";
+import MyFriends from "./features/user/myPage/myFriends/MyFriends";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />}></Route>
+
+      {/* 임시 route. 추후 삭제 예정. */}
+      <Route path="/friend" element={<MyFriends />}></Route>
     </Routes>
   );
 }
