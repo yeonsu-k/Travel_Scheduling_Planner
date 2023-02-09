a071c864185180bd1e39d092b9eef5b277b1951e
jhkang9820 <jhkang9820@gmail.com>
2023-02-09 23:28:24 +0900

    [FE] feat: 메인페이지 여행기 컴포넌트

---
+++ FE/src/features/main/MainTravelLog.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "../main/Main.module.css";
+
+const MainTravelLog = () => {
+  return (
+    <div className={styles.mainTextContainer}>
+      <div className={styles.mainTitleText}>여행기</div>
+      <div className={styles.mainSubTitleText}>TRAVELOG</div>
+      <div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default MainTravelLog;
--- FE/src/pages/MainPage.tsx
+++ FE/src/pages/MainPage.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import styles from "../features/main/Main.module.css";
 import MainDisplay from "../features/main/MainDisplay";
+import MainTravelLog from "../features/main/MainTravelLog";
 
 function MainPage() {
   return (
     <div>
       <MainDisplay />
+      <div className={styles.container}>
+        <div className={styles.containerInner}>
+          <MainTravelLog />
+        </div>
+      </div>
     </div>
   );
 }
