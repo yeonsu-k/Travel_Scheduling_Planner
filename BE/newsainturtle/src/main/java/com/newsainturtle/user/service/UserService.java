2af69c74587eee341c3486d836fed4a55bd7ac56
SungSoo <tjdtn6725@naver.com>
2023-02-16 15:18:52 +0900

    [BE] feature: UserService.java - 이메일 중복 검사 메소드 emailDuplicateCheck 구현

--- BE/newsainturtle/src/main/java/com/newsainturtle/user/service/UserService.java
+++ BE/newsainturtle/src/main/java/com/newsainturtle/user/service/UserService.java
@@ -1,4 +1,27 @@
 package com.newsainturtle.user.service;
 
+import com.newsainturtle.auth.dto.EmailDuplicateCheckRequest;
+import com.newsainturtle.auth.dto.EmailDuplicateCheckResponse;
+import com.newsainturtle.user.entity.User;
+import com.newsainturtle.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public EmailDuplicateCheckResponse emailDuplicateCheck(EmailDuplicateCheckRequest emailDuplicateCheckRequest) {
+        User user = userRepository.findByEmail(emailDuplicateCheckRequest.getEmail());
+        //중복 x -> 가입 가능함
+        if(user == null){
+            return new EmailDuplicateCheckResponse(false);
+        }
+        //중복 o -> 가입 불가능함
+        return new EmailDuplicateCheckResponse(true);
+    }
 }
