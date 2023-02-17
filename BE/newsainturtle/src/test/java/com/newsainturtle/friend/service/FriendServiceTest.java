76ac06335ff073cb51e47a6867b7e5b6ea2a3039
yunaghgh <yunaghgh@naver.com>
2023-02-17 20:16:44 +0900

    [BE] test: 사용자 검색 테스트 중첩

--- BE/newsainturtle/src/test/java/com/newsainturtle/friend/service/FriendServiceTest.java
+++ BE/newsainturtle/src/test/java/com/newsainturtle/friend/service/FriendServiceTest.java
@@ -5,6 +5,7 @@ import com.newsainturtle.friend.dto.UserSearchResponse;
 import com.newsainturtle.user.entity.User;
 import com.newsainturtle.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -23,41 +24,44 @@ class FriendServiceTest {
     @Mock
     private UserRepository userRepository;
 
-    @Test
-    @DisplayName("[성공] - 사용자 없음")
-    void notExistUser() {
-        //given
-        final UserSearchRequest userSearchRequest = UserSearchRequest.builder()
-                .email("yunaghgh@naver.com")
-                .build();
-        doReturn(null).when(userRepository).findByEmail(userSearchRequest.getEmail());
-        //when
-        final UserSearchResponse result = friendService.searchUser(userSearchRequest);
-        //then
-        assertFalse(result.isExist());
+    @Nested
+    @DisplayName("사용자 검색 테스트")
+    class UserSearchTest {
+        @Test
+        @DisplayName("[성공] - 사용자 없음")
+        void notExistUser() {
+            //given
+            final UserSearchRequest userSearchRequest = UserSearchRequest.builder()
+                    .email("yunaghgh@naver.com")
+                    .build();
+            doReturn(null).when(userRepository).findByEmail(userSearchRequest.getEmail());
+            //when
+            final UserSearchResponse result = friendService.searchUser(userSearchRequest);
+            //then
+            assertFalse(result.isExist());
+        }
+
+        @Test
+        @DisplayName("[성공] - 사용자 있음")
+        void existUser() {
+            //given
+            final UserSearchRequest userSearchRequest = UserSearchRequest.builder()
+                    .email("yunaghgh@naver.com")
+                    .build();
+            final User user = User.builder()
+                    .email("yunaghgh@naver.com")
+                    .nickname("Kuuuna98")
+                    .password("pwd1234")
+                    .kakao(false)
+                    .profile("path")
+                    .withdraw(false)
+                    .build();
+
+            doReturn(user).when(userRepository).findByEmail(userSearchRequest.getEmail());
+            //when
+            final UserSearchResponse result = friendService.searchUser(userSearchRequest);
+            //then
+            assertTrue(result.isExist());
+        }
     }
-
-    @Test
-    @DisplayName("[성공] - 사용자 있음")
-    void existUser() {
-        //given
-        final UserSearchRequest userSearchRequest = UserSearchRequest.builder()
-                .email("yunaghgh@naver.com")
-                .build();
-        final User user = User.builder()
-                .email("yunaghgh@naver.com")
-                .nickname("Kuuuna98")
-                .password("pwd1234")
-                .kakao(false)
-                .profile("path")
-                .withdraw(false)
-                .build();
-
-        doReturn(user).when(userRepository).findByEmail(userSearchRequest.getEmail());
-        //when
-        final UserSearchResponse result = friendService.searchUser(userSearchRequest);
-        //then
-        assertTrue(result.isExist());
-    }
-
 }
