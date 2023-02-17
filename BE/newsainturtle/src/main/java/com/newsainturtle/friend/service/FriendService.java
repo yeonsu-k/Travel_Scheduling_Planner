2cdd860615c35660f809f974fa08d4a4f1403095
yunaghgh <yunaghgh@naver.com>
2023-02-17 20:03:46 +0900

    [BE] test: 사용자 검색 시 사용자가 존재하지 않을 때

---
+++ BE/newsainturtle/src/main/java/com/newsainturtle/friend/service/FriendService.java
@@ -0,0 +1,24 @@
+package com.newsainturtle.friend.service;
+
+import com.newsainturtle.friend.dto.UserSearchRequest;
+import com.newsainturtle.friend.dto.UserSearchResponse;
+import com.newsainturtle.user.entity.User;
+import com.newsainturtle.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class FriendService {
+    private final UserRepository userRepository;
+
+    public UserSearchResponse searchUser(UserSearchRequest userSearchRequest) {
+        User user = userRepository.findByEmail(userSearchRequest.getEmail());
+        if (user == null) {
+            return UserSearchResponse.builder().isExist(false).build();
+        }
+        return null;
+    }
+}
