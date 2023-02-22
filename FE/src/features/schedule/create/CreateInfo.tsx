2c75e160b78bb40d78dffe0c6f9e8c97fe557913
dustnzlzl <dustnzlzl@naver.com>
2023-02-23 07:11:24 +0900

    [FE] feat: 일정 선택 목록 탭 추가

--- FE/src/features/schedule/create/CreateInfo.tsx
+++ FE/src/features/schedule/create/CreateInfo.tsx
@@ -2,13 +2,45 @@ import React from "react";
 import InfoLocal from "./info/InfoLocal";
 import InfoDate from "./info/InfoDate";
 import { Stack } from "@mui/system";
+import Text from "components/Text";
+import { Box } from "@mui/material";
+import styles from "./CreateInfo.module.css";
+import InfoListHotel from "./info/InfoListHotel";
+import InfoListPlace from "./info/InfoListPlace";
 
 function CreateInfo() {
+  const [currentTab, setCurrentTab] = React.useState(0);
+
+  const TabArr = [
+    { name: "호텔", content: <InfoListHotel /> },
+    { name: "장소", content: <InfoListPlace /> },
+  ];
+
   return (
     <div style={{ width: "100%", height: "100%" }}>
-      <Stack pt={3} alignItems="center" spacing={1}>
+      <Stack pt={3} alignItems="center">
         <InfoLocal />
         <InfoDate />
+        <Box py={3}>
+          <Text value="선택목록" />
+        </Box>
+        <div className={styles.InfoMenu}>
+          <ul>
+            {TabArr.map((ele, index) => {
+              return (
+                <li
+                  key={index}
+                  className={currentTab === index ? `${styles.focused}` : ""}
+                  onClick={() => setCurrentTab(index)}
+                >
+                  {ele.name}
+                </li>
+              );
+            })}
+          </ul>
+          <div className={currentTab === 0 ? styles.lineLeft : styles.lineRight} />
+        </div>
+        {TabArr[currentTab].content}
       </Stack>
     </div>
   );
