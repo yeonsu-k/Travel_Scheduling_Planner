6f5f6280f13e7a8408c2b20c1f7b712c61796e05
dustnzlzl <dustnzlzl@naver.com>
2023-02-23 03:54:25 +0900

    [FE] feat: 일정생성 기간 선택 기능 추가

--- FE/src/slices/mapSlice.ts
+++ FE/src/slices/mapSlice.ts
@@ -1,19 +1,42 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { addDays, format } from "date-fns";
 
 interface mapConfig {
   place: string;
+  date: {
+    start: string;
+    end: string;
+  };
+}
+
+interface placeConfig {
+  place: string;
+}
+
+interface dateConfig {
+  date: {
+    start: string;
+    end: string;
+  };
 }
 
 const initialState: mapConfig = {
   place: "부산",
+  date: {
+    start: format(new Date(), "yyyy-MM-dd"),
+    end: format(addDays(new Date(), 2), "yyyy-MM-dd"),
+  },
 };
 
 const mapSlice = createSlice({
   name: "map",
   initialState,
   reducers: {
-    setPlace: (state, action: PayloadAction<mapConfig>) => {
+    setPlace: (state, action: PayloadAction<placeConfig>) => {
       state.place = action.payload.place;
+    },
+    setDate: (state, action: PayloadAction<dateConfig>) => {
+      state.date = action.payload.date;
     },
   },
 });
