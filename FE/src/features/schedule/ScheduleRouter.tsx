f471bd7763a6b4d5d26c53bd878d2711ec6004a1
김주연 <jyeon3930@naver.com>
2023-03-01 00:24:38 +0900

    [FE] modify: console.log 삭제

--- FE/src/features/schedule/ScheduleRouter.tsx
+++ FE/src/features/schedule/ScheduleRouter.tsx
@@ -4,7 +4,6 @@ import MapPage from "pages/MapPage";
 import ScheduleEditPage from "pages/ScheduleEditPage";
 
 const ScheduleRouter = () => {
-  console.log("pathname : ", location.pathname);
   return (
     <>
       <Routes>
