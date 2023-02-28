e71e0dc04bf7878adee63ecb9c56fe370fe19eb3
yunaghgh <yunaghgh@naver.com>
2023-02-28 16:57:11 +0900

    [BE] modify: 이메일 유효성 검사 어노테이션 수정

--- BE/newsainturtle/src/main/java/com/newsainturtle/auth/dto/LoginRequest.java
+++ BE/newsainturtle/src/main/java/com/newsainturtle/auth/dto/LoginRequest.java
@@ -4,12 +4,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Getter
 @NoArgsConstructor
 public class LoginRequest {
-    @NotBlank
+    @Email
     private String email;
     @NotBlank
     private String password;
