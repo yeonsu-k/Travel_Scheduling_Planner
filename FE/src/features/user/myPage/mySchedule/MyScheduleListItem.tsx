d084d9df123e7b8a143e4b46bca3a025af4dd532
jhkang9820 <jhkang9820@gmail.com>
2023-03-01 15:55:44 +0900

    [FE] feat: 여행 이름 Input 생성

--- FE/src/features/user/myPage/mySchedule/MyScheduleListItem.tsx
+++ FE/src/features/user/myPage/mySchedule/MyScheduleListItem.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./MySchedule.module.css";
 import Text from "components/Text";
 import Button from "components/Button";
+import Input from "components/Input";
 
 const MyScheduleListItem = () => {
   return (
@@ -21,9 +22,21 @@ const MyScheduleListItem = () => {
             <div className={styles.scheduleContTitle}>
               <div className={styles.scheduleTextTop}>
                 <span className={styles.scheduleTextTitle}>여행이름</span>
-                <span className={styles.scheduleTextCont} style={{ color: "#666" }}>
-                  여행이름
+                <span className={styles.scheduleTextInput} style={{ color: "#666" }}>
+                  <Input placeholder="여행이름" />
                 </span>
+                <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fill="none"
+                    stroke="#999"
+                    d="M18.65,1.68 C18.41,1.45 18.109,1.33 17.81,1.33 C17.499,1.33 17.209,1.45 16.98,1.68 L8.92,9.76 L8,12.33 L10.55,11.41 L18.651,3.34 C19.12,2.87 19.12,2.15 18.65,1.68 L18.65,1.68 L18.65,1.68 Z"
+                  ></path>
+                  <polyline
+                    fill="none"
+                    stroke="#999"
+                    points="16.5 8.482 16.5 18.5 3.5 18.5 3.5 1.5 14.211 1.5"
+                  ></polyline>
+                </svg>
               </div>
               <div className={styles.scheduleTextTop}>
                 <span className={styles.scheduleTextTitle}>최종수정</span>
