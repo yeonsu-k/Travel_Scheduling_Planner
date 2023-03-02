7ccc5695e2611d00e92f84042c88178aa4398517
jhkang9820 <jhkang9820@gmail.com>
2023-03-02 14:45:28 +0900

    [FE] feat: 여행지 아이템 호버 시 이미지 확대 애니메이션 추가

--- FE/src/features/main/MainDestinationsItem.tsx
+++ FE/src/features/main/MainDestinationsItem.tsx
@@ -3,15 +3,13 @@ import styles from "./Main.module.css";
 
 const MainDestinationItem = () => {
   return (
-    <div>
-      <div className={styles.card}>
-        <div>
-          <img className={styles.cardImg} src="https://www.myro.co.kr/myro_image/city/guam.jpg" />
-        </div>
-        <div className={styles.cardContent}>
-          <div className={styles.cardEngTitle}>JEJU</div>
-          <div className={styles.cardTitle}>대한민국 제주도</div>
-        </div>
+    <div className={styles.card}>
+      <div className={styles.cardImgContainer}>
+        <img src="https://www.myro.co.kr/myro_image/city/jeju.jpg" />
+      </div>
+      <div className={styles.cardContent}>
+        <div className={styles.cardEngTitle}>JEJU</div>
+        <div className={styles.cardTitle}>대한민국 제주도</div>
       </div>
     </div>
   );
