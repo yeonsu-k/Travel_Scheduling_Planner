d62d20867e7a049624962c8f43b06ceab6ca2692
SungSoo <tjdtn6725@naver.com>
2023-03-02 13:17:04 +0900

    [BE] feature: UserController.java 마이페이지 조회 컨트롤러 구현

--- BE/newsainturtle/src/main/java/com/newsainturtle/user/controller/UserController.java
+++ BE/newsainturtle/src/main/java/com/newsainturtle/user/controller/UserController.java
@@ -1,4 +1,35 @@
 package com.newsainturtle.user.controller;
 
+
+import com.newsainturtle.common.dto.BaseResponse;
+import com.newsainturtle.common.security.UserDetails;
+import com.newsainturtle.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+import static com.newsainturtle.user.constant.UserConstant.GET_USER_INFO_SUCCESS_MESSAGE;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<BaseResponse> getUserInfo(@ApiIgnore Authentication authentication) throws Exception{
+        UserDetails userDetails = (UserDetails) authentication.getDetails();
+        String email = userDetails.getUsername();
+        return new ResponseEntity<>(BaseResponse.from(
+                true,
+                GET_USER_INFO_SUCCESS_MESSAGE,
+                userService.getUserInfo(email))
+                , HttpStatus.OK);
+
+    }
 }
