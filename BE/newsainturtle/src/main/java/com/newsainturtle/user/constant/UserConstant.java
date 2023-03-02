c6bad4aa84642d530355da16ed91f8e7c6ed96fa
SungSoo <tjdtn6725@naver.com>
2023-03-02 13:15:32 +0900

    [BE] feature: UserConstant.java 마이페이 조회 완료 메시지 상수 작성

---
+++ BE/newsainturtle/src/main/java/com/newsainturtle/user/constant/UserConstant.java
@@ -0,0 +1,8 @@
+package com.newsainturtle.user.constant;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class UserConstant {
+    public static final String GET_USER_INFO_SUCCESS_MESSAGE= "마이페이지 조회가 완료되었습니다.";
+}
