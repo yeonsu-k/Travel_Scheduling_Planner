114d91c42b81cf344f88abd10e9ec75b1fb3cf15
jhkang9820 <jhkang9820@gmail.com>
2023-03-02 16:05:26 +0900

    [FE] feat: 메인페이지 여행지 모달창 생성

--- FE/src/features/main/MainDestinationsItem.tsx
+++ FE/src/features/main/MainDestinationsItem.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import Modal from "@mui/material/Modal";
+import React, { useState } from "react";
+import MainDestinationModal from "./Components/MainDestinationModal";
 import styles from "./Main.module.css";
 
 const MainDestinationItem = () => {
+  const [ModalOpen, setModalOpen] = useState(false);
+  const showModal = () => {
+    setModalOpen(true);
+  };
+
   return (
-    <div className={styles.card}>
-      <div className={styles.cardImgContainer}>
-        <img src="https://www.myro.co.kr/myro_image/city/jeju.jpg" />
+    <div>
+      <div className={styles.card} onClick={showModal}>
+        <div className={styles.cardImgContainer}>
+          <img src="https://www.myro.co.kr/myro_image/city/jeju.jpg" />
+        </div>
+        <div className={styles.cardContent}>
+          <div className={styles.cardEngTitle}>JEJU</div>
+          <div className={styles.cardTitle}>대한민국 제주도</div>
+        </div>
       </div>
-      <div className={styles.cardContent}>
-        <div className={styles.cardEngTitle}>JEJU</div>
-        <div className={styles.cardTitle}>대한민국 제주도</div>
-      </div>
+      {ModalOpen ? (
+        <Modal open={ModalOpen} onClose={() => setModalOpen(false)}>
+          <MainDestinationModal setModalOpen={setModalOpen} />
+        </Modal>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
