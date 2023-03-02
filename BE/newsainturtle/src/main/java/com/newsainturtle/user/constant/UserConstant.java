f903b62ce5e904300263d6d4aa7ff5238964aa49
SungSoo <tjdtn6725@naver.com>
2023-03-02 15:50:13 +0900

    [BE] feature: UserConstant.java 회원정보 수정시 기존 비밀번호와 일치하지 않는 경우 상수 메세지 작성

--- BE/newsainturtle/src/main/java/com/newsainturtle/user/constant/UserConstant.java
+++ BE/newsainturtle/src/main/java/com/newsainturtle/user/constant/UserConstant.java
@@ -6,4 +6,6 @@ import lombok.NoArgsConstructor;
 public class UserConstant {
     public static final String GET_USER_INFO_SUCCESS_MESSAGE= "마이페이지 조회가 완료되었습니다.";
     public static final String  MODIFY_USER_PROFILE_SUCCESS_MESSAGE = "프로필 사진 수정이 완료되었습니다.";
+    public static final String MODIFY_USER_INFO_SUCCESS_MESSAGE = "유저 정보 수정이 완료되었습니다";
+    public static final String NOT_EQUALS_PASSWORD_MESSAGE = "기존의 비밀번호가 일치하지 않습니다";
 }
