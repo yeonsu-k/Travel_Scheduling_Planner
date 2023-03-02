e7f50a43b7abf7a8828293b2e41297eb621232ef
dustnzlzl <dustnzlzl@naver.com>
2023-03-03 02:57:46 +0900

    [FE] refactor: 호텔,장소 검색 부분 리팩토리

--- FE/src/features/schedule/create/CreateSearch.tsx
+++ FE/src/features/schedule/create/CreateSearch.tsx
@@ -2,15 +2,23 @@ import React from "react";
 import styles from "./CreateInfo.module.css";
 import SearchRadioBtn from "./search/SearchRadioBtn";
 import SearchInput from "./search/SearchInput";
-import { Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import SearchList from "./search/SearchList";
+import Text from "components/Text";
 
 function CreateRight() {
   return (
-    <Stack className={styles.Container} spacing={1}>
-      <SearchRadioBtn />
-      <SearchInput />
-      <SearchList />
+    <Stack className={styles.Container}>
+      <Stack spacing={1}>
+        <SearchRadioBtn />
+        <SearchInput />
+      </Stack>
+      <Box className={styles.center} my={1.5}>
+        <Text value="추천 호텔" bold />
+      </Box>
+      <div className={styles.scroll}>
+        <SearchList />
+      </div>
     </Stack>
   );
 }
--- FE/src/features/schedule/create/search/SearchList.tsx
+++ FE/src/features/schedule/create/search/SearchList.tsx
@@ -1,25 +1,19 @@
-import Text from "components/Text";
 import React, { useEffect, useState } from "react";
-import styles from "./Search.module.css";
 import SearchListCard from "./SearchListCard";
-import { Box } from "@mui/material";
 
 function SearchList() {
   const [list, setCardList] = useState<{ id: number; image: string; name: string }[]>([]);
 
   return (
-    <div>
-      <Box className={styles.center} my={1}>
-        <Text value="추천 호텔" bold />
-      </Box>
-      {list.map((card, idx) => {
+    <>
+      {list.map((card, index) => {
         return (
-          <>
-            <SearchListCard id={card.id} image={card.image} name={card.name} key={idx} />
-          </>
+          <div key={index}>
+            <SearchListCard id={card.id} image={card.image} name={card.name} />
+          </div>
         );
       })}
-    </div>
+    </>
   );
 }
 
