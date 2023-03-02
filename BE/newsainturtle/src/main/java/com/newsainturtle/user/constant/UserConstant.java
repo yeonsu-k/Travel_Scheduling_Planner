fcf6d03668902b2f69f81de26cce54011e99cf53
SungSoo <tjdtn6725@naver.com>
2023-03-02 14:19:11 +0900

    [BE] feature: UserConstant.java 프로필 사진 수정 완료 응답 메세지 상수

--- BE/newsainturtle/src/main/java/com/newsainturtle/user/constant/UserConstant.java
+++ BE/newsainturtle/src/main/java/com/newsainturtle/user/constant/UserConstant.java
@@ -5,4 +5,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserConstant {
     public static final String GET_USER_INFO_SUCCESS_MESSAGE= "마이페이지 조회가 완료되었습니다.";
+    public static final String  MODIFY_USER_PROFILE_SUCCESS_MESSAGE = "프로필 사진 수정이 완료되었습니다.";
 }
