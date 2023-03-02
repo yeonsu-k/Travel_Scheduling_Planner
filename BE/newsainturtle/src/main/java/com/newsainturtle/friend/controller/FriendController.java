6248167683f3acea21d5df1a30b342e74ea40e6e
yunaghgh <yunaghgh@naver.com>
2023-03-02 15:44:30 +0900

    [BE] modify: 사용자 검색 관련 프로덕션 코드 수정

--- BE/newsainturtle/src/main/java/com/newsainturtle/friend/controller/FriendController.java
+++ BE/newsainturtle/src/main/java/com/newsainturtle/friend/controller/FriendController.java
@@ -1,20 +1,21 @@
 package com.newsainturtle.friend.controller;
 
 import com.newsainturtle.common.dto.BaseResponse;
+import com.newsainturtle.common.security.UserDetails;
 import com.newsainturtle.friend.dto.UserSearchRequest;
 import com.newsainturtle.friend.dto.UserSearchResponse;
 import com.newsainturtle.friend.service.FriendService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
 
@@ -29,8 +30,11 @@ public class FriendController {
 
     @PostMapping("/search")
     @ApiOperation(value = "사용자 검색", notes = "친구 요청을 위한 사용자 검색")
-    public ResponseEntity<BaseResponse> searchUser(@RequestBody @Valid @ApiParam(value = "사용자 이메일", required = true)final UserSearchRequest userSearchRequest){
-        UserSearchResponse userSearchResponse = friendService.searchUser(userSearchRequest);
+    public ResponseEntity<BaseResponse> searchUser(@ApiIgnore Authentication authentication,
+                                                   @RequestBody @Valid @ApiParam(value = "사용자 이메일", required = true)final UserSearchRequest userSearchRequest){
+        UserDetails userDetails = (UserDetails) authentication.getDetails();
+        String email = userDetails.getUsername();
+        UserSearchResponse userSearchResponse = friendService.searchUser(email, userSearchRequest);
         return new ResponseEntity<>(BaseResponse.from(
                 true,
                 FRIEND_SEARCH_SUCCESS_MESSAGE,
--- BE/newsainturtle/src/main/java/com/newsainturtle/friend/service/FriendService.java
+++ BE/newsainturtle/src/main/java/com/newsainturtle/friend/service/FriendService.java
@@ -2,6 +2,8 @@ package com.newsainturtle.friend.service;
 
 import com.newsainturtle.friend.dto.UserSearchRequest;
 import com.newsainturtle.friend.dto.UserSearchResponse;
+import com.newsainturtle.friend.entity.Friend;
+import com.newsainturtle.friend.repository.FriendRepository;
 import com.newsainturtle.user.entity.User;
 import com.newsainturtle.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,17 +15,38 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class FriendService {
     private final UserRepository userRepository;
+    private final FriendRepository friendRepository;
 
-    public UserSearchResponse searchUser(UserSearchRequest userSearchRequest) {
+    public UserSearchResponse searchUser(String email, UserSearchRequest userSearchRequest) {
         User user = userRepository.findByEmail(userSearchRequest.getEmail());
         if (user == null || user.isWithdraw()) {
             return UserSearchResponse.builder().isExist(false).build();
         }
+        String status = checkUserRelationships(email,user);
         return UserSearchResponse.builder()
                 .isExist(true)
                 .email(user.getEmail())
                 .nickname(user.getNickname())
                 .profile(user.getProfile())
+                .status(status)
                 .build();
+    }
+
+    String checkUserRelationships(String email, User receiveUser){
+        if(receiveUser.getEmail().equals(email)){
+            return  "본인";
+        }else {
+            User requestUser = userRepository.findByEmail(email);
+            Friend friend = friendRepository.findByRequestUserAndReceiveUser(requestUser,receiveUser);
+            if(friend == null){
+                friend = friendRepository.findByRequestUserAndReceiveUser(receiveUser,requestUser);
+                if(friend == null || !friend.isAccept()){
+                    return "친구요청";
+                }
+            }else if(!friend.isAccept()){
+                return "요청완료";
+            }
+        }
+        return "친구";
     }
 }
