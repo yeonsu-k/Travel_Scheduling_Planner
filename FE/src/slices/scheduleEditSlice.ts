1408711d60ccef1896975c2c1a22f789db2cf7a2
김주연 <jyeon3930@naver.com>
2023-03-05 00:27:04 +0900

    [FE] modify: 일정 수정 slice 코드 수정

--- FE/src/slices/scheduleEditSlice.ts
+++ FE/src/slices/scheduleEditSlice.ts
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import { rootState } from "app/store";
 
 interface placeInfoConfig {
+  id: number;
   img: string;
   placeName: string;
   time: string;
@@ -25,6 +26,7 @@ const initialState: scheduleEditConfig = {
       day: 1,
       dayList: [
         {
+          id: 1,
           img: "src",
           placeName: "제주공항",
           time: "1시간 0분",
@@ -32,6 +34,7 @@ const initialState: scheduleEditConfig = {
           endTime: "11:00",
         },
         {
+          id: 2,
           img: "src",
           placeName: "성산 일출봉",
           time: "2시간 0분",
@@ -39,6 +42,7 @@ const initialState: scheduleEditConfig = {
           endTime: "15:00",
         },
         {
+          id: 3,
           img: "src",
           placeName: "우도",
           time: "2시간 0분",
@@ -51,6 +55,7 @@ const initialState: scheduleEditConfig = {
       day: 2,
       dayList: [
         {
+          id: 4,
           img: "src",
           placeName: "협재 해수욕장",
           time: "1시간 0분",
@@ -58,6 +63,7 @@ const initialState: scheduleEditConfig = {
           endTime: "11:00",
         },
         {
+          id: 5,
           img: "src",
           placeName: "도두 무지개 해안도로",
           time: "1시간 0분",
@@ -70,6 +76,7 @@ const initialState: scheduleEditConfig = {
       day: 3,
       dayList: [
         {
+          id: 6,
           img: "src",
           placeName: "쇠소깍",
           time: "2시간 0분",
@@ -77,23 +84,26 @@ const initialState: scheduleEditConfig = {
           endTime: "12:00",
         },
         {
+          id: 7,
           img: "src",
           placeName: "카멜리아 힐",
           time: "1시간 0분",
           startTime: "14:00",
           endTime: "15:00",
         },
-        {
-          img: "src",
-          placeName: "제주공항",
-          time: "0시간 0분",
-          startTime: "16:00",
-          endTime: "16:00",
-        },
       ],
     },
   ],
-  keepPlaceList: [],
+  keepPlaceList: [
+    {
+      id: 8,
+      img: "src",
+      placeName: "월정리 해수욕장",
+      time: "2시간 0분",
+      startTime: "10:00",
+      endTime: "12:00",
+    },
+  ],
 };
 
 const scheduleEditSlice = createSlice({
