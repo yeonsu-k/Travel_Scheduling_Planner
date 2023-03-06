485b35ba14fc8f4d08ce95d623ccbe84aaa09a8b
김주연 <jyeon3930@naver.com>
2023-03-07 05:42:14 +0900

    [FE] feat: 알림 내역이 없을 시 렌더링되는 화면 추가

--- FE/src/features/user/notice/Notice.tsx
+++ FE/src/features/user/notice/Notice.tsx
@@ -16,13 +16,19 @@ const Notice = () => {
 
   return (
     <div className={styles.notice}>
-      <div className={styles.clearBtn}>
-        <Button text="전체 삭제" color="main" radius width="8vw" height="4vh" onClick={onClickClearBtn} />
-      </div>
+      {noticeList.length === 0 ? (
+        <div>알림 내역이 없습니다.</div>
+      ) : (
+        <>
+          <div className={styles.clearBtn}>
+            <Button text="전체 삭제" color="main" radius width="8vw" height="4vh" onClick={onClickClearBtn} />
+          </div>
 
-      {noticeList.map((value, key) => (
-        <NoticeItem key={key} noticeValue={value} />
-      ))}
+          {noticeList.map((value, key) => (
+            <NoticeItem key={key} noticeValue={value} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
