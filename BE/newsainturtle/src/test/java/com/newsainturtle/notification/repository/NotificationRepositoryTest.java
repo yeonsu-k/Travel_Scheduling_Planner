69ac2acdf1dd1fcc7b15c0411729d6c06838de98
yunaghgh <yunaghgh@naver.com>
2023-03-07 22:30:47 +0900

    [BE] test: 알림 개별 삭제 레포지토리 테스트코드 작성

--- BE/newsainturtle/src/test/java/com/newsainturtle/notification/repository/NotificationRepositoryTest.java
+++ BE/newsainturtle/src/test/java/com/newsainturtle/notification/repository/NotificationRepositoryTest.java
@@ -14,7 +14,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @DisplayName("알림 레포지토리 테스트")
@@ -75,6 +75,99 @@ class NotificationRepositoryTest {
 
             //then
             assertEquals(result.size(), 3);
+        }
+    }
+
+    @Nested
+    @DisplayName("알림 개별 삭제")
+    class DeleteNotification {
+        @Test
+        @DisplayName("사용자 소유의 알림이 아님")
+        void 사용자알림이아님() {
+            //given
+            final User user = User.builder()
+                    .email("test@naver.com")
+                    .nickname("Kuuuna98")
+                    .password("pwd1234")
+                    .kakao(false)
+                    .profile("path")
+                    .withdraw(false)
+                    .build();
+            final User sender = User.builder()
+                    .email("test11@naver.com")
+                    .nickname("johnny")
+                    .password("pwd127")
+                    .kakao(false)
+                    .profile("path")
+                    .withdraw(false)
+                    .build();
+            final Notification friendNotification1 = FriendNotification.builder()
+                    .receiveUser(user)
+                    .sendUserId(sender.getUserId())
+                    .notificationStatus(Notification.Status.ACCEPT)
+                    .build();
+
+            userRepository.save(user);
+            userRepository.save(sender);
+            notificationRepository.save(friendNotification1);
+
+            //when
+            Notification result = notificationRepository.findByNotificationIdAndReceiveUser(friendNotification1.getNotificationId(), sender);
+            //then
+            assertNull(result);
+        }
+
+        @Test
+        @DisplayName("알림 삭제")
+        void 알림삭제() {
+            //given
+            final User user = User.builder()
+                    .email("test@naver.com")
+                    .nickname("Kuuuna98")
+                    .password("pwd1234")
+                    .kakao(false)
+                    .profile("path")
+                    .withdraw(false)
+                    .build();
+            final User sender = User.builder()
+                    .email("test127@naver.com")
+                    .nickname("johnny")
+                    .password("pwd127")
+                    .kakao(false)
+                    .profile("path")
+                    .withdraw(false)
+                    .build();
+            final Notification friendNotification1 = FriendNotification.builder()
+                    .receiveUser(user)
+                    .sendUserId(sender.getUserId())
+                    .notificationStatus(Notification.Status.ACCEPT)
+                    .build();
+            final Notification scheduleNotification1 = ScheduleNotification.builder()
+                    .receiveUser(user)
+                    .sendUserId(sender.getUserId())
+                    .notificationStatus(Notification.Status.NO_RESPONSE)
+                    .scheduleId(1L)
+                    .build();
+            final Notification scheduleNotification2 = ScheduleNotification.builder()
+                    .receiveUser(user)
+                    .sendUserId(sender.getUserId())
+                    .notificationStatus(Notification.Status.REJECT)
+                    .scheduleId(2L)
+                    .build();
+            userRepository.save(user);
+            notificationRepository.save(friendNotification1);
+            notificationRepository.save(scheduleNotification1);
+            notificationRepository.save(scheduleNotification2);
+
+            //when
+            Notification result1 = notificationRepository.findByNotificationIdAndReceiveUser(scheduleNotification1.getNotificationId(), user);
+            notificationRepository.deleteByNotificationIdAndReceiveUser(scheduleNotification1.getNotificationId(), user);
+            Notification result2 = notificationRepository.findByNotificationIdAndReceiveUser(scheduleNotification1.getNotificationId(), user);
+            //then
+            assertAll(
+                    () -> assertNotNull(result1),
+                    () -> assertNull(result2)
+            );
         }
     }
 }
