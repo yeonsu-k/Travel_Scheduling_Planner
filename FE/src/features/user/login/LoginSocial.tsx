be6d4c13099f69cd49907bab0bbba8008ff8c62a
jhkang9820 <jhkang9820@gmail.com>
2023-03-07 14:56:34 +0900

    [FE] design: 카카오 로그인만 가능

--- FE/src/features/user/login/LoginSocial.tsx
+++ FE/src/features/user/login/LoginSocial.tsx
@@ -10,12 +10,12 @@ const LoginSNS = () => {
         <div className={styles.socialBtn}>
           <img src="https://www.myro.co.kr/myro_image/kakaolink_btn.png" />
         </div>
-        <div className={styles.socialBtn}>
+        {/* <div className={styles.socialBtn}>
           <img src="https://www.myro.co.kr/myro_image/naver_btn.png" />
         </div>
         <div className={styles.socialBtn}>
           <img src="https://www.myro.co.kr/myro_image/google_btn.png" />
-        </div>
+        </div> */}
       </div>
     </div>
   );
