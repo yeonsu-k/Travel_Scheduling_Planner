47706d38cabf1894c957190392d6e2417eee3801
dustnzlzl <dustnzlzl@naver.com>
2023-03-07 18:06:40 +0900

    [FE] style: 호텔 선택 리스트 컴포넌트 코드 스타일 변경

--- FE/src/features/schedule/create/info/InfoListHotel.tsx
+++ FE/src/features/schedule/create/info/InfoListHotel.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { basicConfig, selectDate, selectHotelList, setHotelList } from "slices/scheduleCreateSlice";
+import React, { useEffect, useMemo, useState } from "react";
+import { basicConfig, selectDate, selectHotelList, setdeleteHotel, setHotelList } from "slices/scheduleCreateSlice";
 import { useAppDispatch, useAppSelector } from "app/hooks";
 import styles from "./Info.module.css";
 import Text from "components/Text";
@@ -11,99 +11,99 @@ function InfoListHotel() {
   const dispatch = useAppDispatch();
   const hotel = useAppSelector(selectHotelList);
   const date = useAppSelector(selectDate);
-  const [currentDay, setCurrentDay] = React.useState(0);
-  const [hotelDays, setHotelDays] = React.useState<(basicConfig | null)[]>([]);
+  const [currentDay, setCurrentDay] = useState(0);
+  // const [hotelDays, setHotelDays] = useState<(basicConfig | null)[]>(() => {
+  //   const size = differenceInDays(new Date(date.end), new Date(date.start));
+  //   return Array.from({ length: size }, () => null);
+  // });
 
-  const size = differenceInDays(new Date(date.end), new Date(date.start));
+  // useEffect(() => {
+  //   setHotelDays([...hotel]);
+  // }, [hotel]);
+
   useEffect(() => {
-    setHotelDays([...hotel]);
-  }, [hotel]);
+    console.log(
+      hotel.map((hotels, index) => {
+        hotels;
+      }),
+    );
+  }, []);
 
   const deleteHotel = (id: number | undefined) => {
-    const index = hotelDays.findIndex((hotelDays) => hotelDays?.id === id);
-    setCurrentDay(index);
-    const copy = [...hotelDays];
-    copy[index] = null;
-    setHotelDays(copy);
-    dispatch(setHotelList(copy));
+    // const index = hotelDays.findIndex((hotelDays) => hotelDays?.id === id);
+    // setCurrentDay(index);
+    // const copy = [...hotelDays];
+    // copy[index] = null;
+    // setHotelDays(copy);
+    dispatch(setdeleteHotel({ id: id }));
   };
 
   const deleteHotelAll = () => {
     setCurrentDay(0);
-    setHotelDays([]);
-    dispatch(setHotelList([]));
-  };
-
-  const rendering = () => {
-    const result = [];
-    for (let index = 0; index < size; index++) {
-      result.push(
-        <Box className={styles.flex} mb={1} key={index}>
-          <button
-            className={currentDay === index ? `${styles.days_btn} ${styles.days_focused}` : `${styles.days_btn}`}
-            onClick={() => setCurrentDay(index)}
-          >
-            DAY{index + 1}
-            &nbsp;&nbsp;
-            <small>
-              {format(addDays(new Date(date.start), index), "MM.dd")}
-              {" - "}
-              {format(addDays(new Date(date.start), index + 1), "MM.dd")}
-            </small>
-          </button>
-          <div className={styles.cardList}>
-            <div className={styles.card}>
-              {hotelDays[index] ? (
-                <>
-                  <img src={hotelDays[index]?.image} />
-                  <div>
-                    <span className={styles.cardText}>
-                      {hotelDays[index]?.name}
-                      <br />
-                      <small>Maison Glad Jeju</small>
-                    </span>
-                    <div className={styles.cardDelete}>
-                      <button onClick={() => deleteHotel(hotelDays[index]?.id)}>
-                        <Close fontSize="small" color="error" />
-                      </button>
-                    </div>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <Box my={0.5}>
-                    <p className={styles.explain}>일자 버튼을 누르고 호텔을 추가하세요.</p>
-                  </Box>
-                </>
-              )}
-            </div>
-          </div>
-        </Box>,
-      );
-    }
-    return result;
+    // setHotelDays([]);
+    const size = differenceInDays(new Date(date.end), new Date(date.start));
+    dispatch(setHotelList(Array.from({ length: size }, () => null)));
   };
 
   return (
-    <>
-      <div className={styles.flex}>
-        <Box my={2.5}>
-          <Text
-            value={hotelDays.filter((element) => null != element).length.toString()}
-            type="textTitle"
-            color="yellow"
-            en
-          />
-        </Box>
-        <button onClick={deleteHotelAll} className={`${styles.btn} ${styles.delete_btn}`}>
-          호텔전체삭제
-        </button>
-        <Box my={0.5}>
-          <p className={styles.explain}>숙소는 일정의 시작 지점과 종료 지점으로 설정됩니다.</p>
-        </Box>
-        {rendering()}
-      </div>
-    </>
+    <div className={styles.flex}>
+      <Box my={2.5}>
+        <Text value={hotel.filter((element) => null != element).length.toString()} type="textTitle" color="yellow" en />
+      </Box>
+      <button onClick={deleteHotelAll} className={`${styles.btn} ${styles.delete_btn}`}>
+        호텔전체삭제
+      </button>
+      <Box my={0.5}>
+        <p className={styles.explain}>숙소는 일정의 시작 지점과 종료 지점으로 설정됩니다.</p>
+      </Box>
+      <>
+        {hotel.map((hotelCard, index) => (
+          <Box className={styles.flex} mb={1} key={index}>
+            <button
+              className={currentDay === index ? `${styles.days_btn} ${styles.days_focused}` : `${styles.days_btn}`}
+              onClick={() => setCurrentDay(index)}
+            >
+              DAY{index + 1}
+              &nbsp;&nbsp;
+              <small>
+                {format(addDays(new Date(date.start), index), "MM.dd")}
+                {" - "}
+                {format(addDays(new Date(date.start), index + 1), "MM.dd")}
+              </small>
+            </button>
+            <div className={styles.cardList}>
+              <div className={styles.card}>
+                {hotelCard != null ? (
+                  <>
+                    <img src={hotelCard.image} />
+                    <div>
+                      <span className={styles.cardText}>
+                        {hotelCard.name}
+                        <br />
+                        <small>Maison Glad Jeju</small>
+                      </span>
+                      <div className={styles.cardDelete}>
+                        <button onClick={() => deleteHotel(hotelCard.id)}>
+                          <Close fontSize="small" color="error" />
+                        </button>
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <Box my={0.5}>
+                      <div onClick={() => setCurrentDay(index)} style={{ cursor: "pointer" }}>
+                        <p className={styles.explain}>일자 버튼을 누르고 호텔을 추가하세요.</p>
+                      </div>
+                    </Box>
+                  </>
+                )}
+              </div>
+            </div>
+          </Box>
+        ))}
+      </>
+    </div>
   );
 }
 
