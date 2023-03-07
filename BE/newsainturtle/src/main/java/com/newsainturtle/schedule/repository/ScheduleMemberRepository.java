41fa5788968f0759c9f51abf0bb828d0463bec09
SungSoo <tjdtn6725@naver.com>
2023-03-07 14:56:53 +0900

    [BE] feat: ScheduleMemberRepository.java 일정에 포함된 멤버 레포지토리 인터페이스 작성

---
+++ BE/newsainturtle/src/main/java/com/newsainturtle/schedule/repository/ScheduleMemberRepository.java
@@ -0,0 +1,10 @@
+package com.newsainturtle.schedule.repository;
+
+import com.newsainturtle.schedule.entity.ScheduleMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ScheduleMemberRepository extends JpaRepository<ScheduleMember,Long> {
+    List<ScheduleMember> findAllByUserEmail(String email);
+}
