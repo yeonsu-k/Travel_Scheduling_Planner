2ba33a065714981c75dfc49ce79c85178f80b6c4
yunaghgh <yunaghgh@naver.com>
2023-03-07 22:32:43 +0900

    [BE] feat: 알림 개별 삭제 API 작성

--- BE/newsainturtle/src/main/java/com/newsainturtle/notification/controller/NotificationController.java
+++ BE/newsainturtle/src/main/java/com/newsainturtle/notification/controller/NotificationController.java
@@ -9,11 +9,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
+import static com.newsainturtle.notification.constant.NotificationConstant.REMOVE_NOTIFICATION_ITEM_SUCCESS_MESSAGE;
 import static com.newsainturtle.notification.constant.NotificationConstant.SELECT_NOTIFICATION_LIST_SUCCESS_MESSAGE;
 
 @RestController
@@ -25,7 +24,7 @@ public class NotificationController {
 
     @GetMapping("")
     @ApiOperation(value = "알림 조회", notes = "사용자가 받은 알림 목록 조회")
-    public ResponseEntity<BaseResponse> selectNotificationList(@ApiIgnore Authentication authentication){
+    public ResponseEntity<BaseResponse> selectNotificationList(@ApiIgnore Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getDetails();
         String email = userDetails.getUsername();
         NotificationListResponse notificationListResponse = notificationService.selectNotificationList(email);
@@ -33,6 +32,19 @@ public class NotificationController {
                 true,
                 SELECT_NOTIFICATION_LIST_SUCCESS_MESSAGE,
                 notificationListResponse)
+                , HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{notification_id}")
+    @ApiOperation(value = "알림 개별 삭제", notes = "{notification_id} 알림 삭제")
+    public ResponseEntity<BaseResponse> removeNotification(@ApiIgnore Authentication authentication,
+                                                           @PathVariable(name = "notification_id") final Long notificationId) {
+        UserDetails userDetails = (UserDetails) authentication.getDetails();
+        String email = userDetails.getUsername();
+        notificationService.removeNotification(email, notificationId);
+        return new ResponseEntity<>(BaseResponse.from(
+                true,
+                REMOVE_NOTIFICATION_ITEM_SUCCESS_MESSAGE)
                 , HttpStatus.OK);
     }
 }
