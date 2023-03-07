42fc85dcf8d96204e8f3354ff4f026724ed8db27
tgb02087 <tgb02087@naver.com>
2023-03-07 17:03:01 +0900

    [BE] feature: ScheduleLocation Null Message 추가

--- BE/newsainturtle/src/main/java/com/newsainturtle/schedule/constant/ScheduleErrorConstant.java
+++ BE/newsainturtle/src/main/java/com/newsainturtle/schedule/constant/ScheduleErrorConstant.java
@@ -8,5 +8,7 @@ public class ScheduleErrorConstant {
 
     public static final String NULL_ERROR_MESSAGE = "데이터가 NULL 입니다.";
 
+    public static final String NULL_SCHEDULE_LOCATION_MESSAGE = "일정 장소가 없습니다.";
+
     public static final String REGION_DUPLICATE_MESSAGE = "지역 등록이 중복되었습니다.";
 }
