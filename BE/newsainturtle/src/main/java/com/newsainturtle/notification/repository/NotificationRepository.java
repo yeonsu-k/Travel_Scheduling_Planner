fb7f112e744e7ceb7b92ad468c83b51e73323425
yunaghgh <yunaghgh@naver.com>
2023-03-07 15:15:28 +0900

    [BE] feat: 알림 조회 레포지토리 프로덕션코드 작성

--- BE/newsainturtle/src/main/java/com/newsainturtle/notification/repository/NotificationRepository.java
+++ BE/newsainturtle/src/main/java/com/newsainturtle/notification/repository/NotificationRepository.java
@@ -1,4 +1,11 @@
 package com.newsainturtle.notification.repository;
 
-public class NotificationRepository {
+import com.newsainturtle.notification.entity.Notification;
+import com.newsainturtle.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByReceiveUser(User user);
 }
