e2060093614d57e4766c22bb6bcd78962a389183
tgb02087 <tgb02087@naver.com>
2023-03-09 17:36:04 +0900

    [BE] create: ScheduleLocationResponse Dto 생성

---
+++ BE/newsainturtle/src/main/java/com/newsainturtle/schedule/dto/ScheduleLocationResponse.java
@@ -0,0 +1,19 @@
+package com.newsainturtle.schedule.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ScheduleLocationResponse {
+
+    private LocationResponse location;
+
+    private Long day;
+
+    private Long sequence;
+
+    private String startTime;
+
+    private String endTime;
+}
