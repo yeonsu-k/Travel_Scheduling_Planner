cbc171e96cd55e6a4020996cf5a6795a88c4b1bc
SungSoo <tjdtn6725@naver.com>
2023-03-14 13:51:56 +0900

    [BE] feat: UserController.java 스웨거 설정 추가

--- BE/newsainturtle/src/main/java/com/newsainturtle/user/controller/UserController.java
+++ BE/newsainturtle/src/main/java/com/newsainturtle/user/controller/UserController.java
@@ -7,6 +7,9 @@ import com.newsainturtle.user.dto.ModifyProfileRequest;
 import com.newsainturtle.user.dto.ModifyScheduleNameRequest;
 import com.newsainturtle.user.dto.ModifyUserInfoRequest;
 import com.newsainturtle.user.service.UserService;
+import io.swagger.annotations.Api;
+
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +22,11 @@ import static com.newsainturtle.user.constant.UserConstant.*;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
+@Api(description = "마이페이지 및 유저 관련 컨트롤러")
 public class UserController {
     private final UserService userService;
 
+    @Operation(summary = "사용자 정보 조회", description = "Access Token을 받아 사용자 정보를 조회합니다.")
     @GetMapping
     public ResponseEntity<BaseResponse> getUserInfo(@ApiIgnore Authentication authentication) throws Exception {
         UserDetails userDetails = (UserDetails) authentication.getDetails();
@@ -33,6 +38,9 @@ public class UserController {
                 , HttpStatus.OK);
 
     }
+
+
+    @Operation(summary = "사용자 정보 수정", description = "사용자 정보를 수정합니다.")
     @PostMapping
     public ResponseEntity<BaseResponse> modifyUserInfo(@ApiIgnore Authentication authentication, @RequestBody final ModifyUserInfoRequest modifyUserInfoRequest) throws Exception {
         UserDetails userDetails = (UserDetails) authentication.getDetails();
@@ -45,6 +53,7 @@ public class UserController {
 
     }
 
+    @Operation(summary = "프로필 사진 수정", description = "프로필 사진을 수정합니다.")
     @PostMapping("/profile")
     public ResponseEntity<BaseResponse> modifyProfile(@ApiIgnore Authentication authentication, @RequestBody final ModifyProfileRequest modifyProfileRequest) throws Exception {
         UserDetails userDetails = (UserDetails) authentication.getDetails();
@@ -55,6 +64,8 @@ public class UserController {
                 userService.modifyProfile(email, modifyProfileRequest.getPath()))
                 , HttpStatus.OK);
     }
+
+    @Operation(summary = "일정 이름 수정", description = "일정 이름을 수정합니다.")
     @PostMapping("/{schedule_id}")
     public ResponseEntity<BaseResponse> modifyScheduleName(@ApiIgnore Authentication authentication, @RequestBody final ModifyScheduleNameRequest modifyScheduleNameRequest, @PathVariable("schedule_id") Long schedule_id) {
         userService.modifyScheduleName(modifyScheduleNameRequest.getSchedule_name(),schedule_id);
@@ -62,6 +73,8 @@ public class UserController {
                 BaseResponse.from(true, MODIFY_SCHEDULE_NAME_SUCCESS_MESSAGE),
                 HttpStatus.OK);
     }
+
+    @Operation(summary = "일정 목록 조회", description = "일정 목록을 조회합니다.")
     @GetMapping("/schedule_list")
     public ResponseEntity<BaseResponse> getScheduleList(@ApiIgnore Authentication authentication) throws Exception{
         UserDetails userDetails = (UserDetails) authentication.getDetails();
@@ -73,6 +86,8 @@ public class UserController {
         ), HttpStatus.OK);
     }
 
+
+    @Operation(summary = "일정 공개 여부 수정", description = "일정 공개 여부를 수정합니다.")
     @PostMapping("/open/{schedule_id}")
     public ResponseEntity<BaseResponse> modifyIsPrivate(@ApiIgnore Authentication authentication, @PathVariable("schedule_id") Long schedule_id) {
         userService.modifyScheduleIsPrivate(schedule_id);
