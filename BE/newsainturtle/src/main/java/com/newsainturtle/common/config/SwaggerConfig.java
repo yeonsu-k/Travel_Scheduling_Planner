295ccc935e7120cb114b2f533b7512548f8f8a04
SungSoo <tjdtn6725@naver.com>
2023-03-14 13:51:33 +0900

    [BE] feat: SwaggerConfig.java Swagger 설정 추가

--- BE/newsainturtle/src/main/java/com/newsainturtle/common/config/SwaggerConfig.java
+++ BE/newsainturtle/src/main/java/com/newsainturtle/common/config/SwaggerConfig.java
@@ -6,23 +6,26 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.Server;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Arrays;
 import java.util.Collections;
-
+import java.util.List;
 @Configuration
 @EnableOpenApi
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        Server serverLocal = new Server("local", "http://localhost:8080", "for local usages", Collections.emptyList(),
+        Server serverLocal = new Server("local", "http://localhost:8080/", "for local usages", Collections.emptyList(),
                 Collections.emptyList());
 
         return new Docket(DocumentationType.OAS_30)
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .servers(serverLocal)
                 .groupName("api")
                 .apiInfo(this.apiInfo())
@@ -38,5 +41,22 @@ public class SwaggerConfig {
                 .description("REST API")
                 .version("1.0")
                 .build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
     }
 }
