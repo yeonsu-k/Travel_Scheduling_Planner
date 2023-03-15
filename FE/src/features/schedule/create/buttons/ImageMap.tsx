9648c2666c869eefa82dce805521e94a73f07b10
dustnzlzl <dustnzlzl@naver.com>
2023-03-15 16:12:39 +0900

    [FE] feat: 주소 검색으로 장소추가 이미지 지도로 보여주기

--- FE/src/features/schedule/create/buttons/ImageMap.tsx
+++ FE/src/features/schedule/create/buttons/ImageMap.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 interface ImageMapType {
   address: string;
-  setAddressSearch: React.Dispatch<React.SetStateAction<boolean>>;
+  setOpenSeachToast: React.Dispatch<React.SetStateAction<boolean>>;
+  setSearchBtnClick: React.Dispatch<React.SetStateAction<boolean>>;
+  setAddSuccess: React.Dispatch<React.SetStateAction<boolean>>;
+  setGps: React.Dispatch<
+    React.SetStateAction<{
+      longitude: number;
+      latitude: number;
+    }>
+  >;
 }
 
 declare global {
@@ -13,29 +21,51 @@ declare global {
 
 const { kakao } = window;
 
-function ImageMap({ address, setAddressSearch }: ImageMapType) {
-  const geocoder = new kakao.maps.services.Geocoder();
-  geocoder.addressSearch(address, function (result: any[], status: string) {
-    if (status === kakao.maps.services.Status.OK) {
-      if (result.length > 0) {
-        setAddressSearch(true);
+function ImageMap(props: ImageMapType) {
+  const { address, setSearchBtnClick, setOpenSeachToast, setAddSuccess, setGps } = props;
+  const imgMap = useRef<HTMLInputElement>(null);
+  function setMap() {
+    const container = document.getElementById("ImgMap");
+    const geocoder = new kakao.maps.services.Geocoder();
+
+    geocoder.addressSearch(address, function (result: any[], status: string) {
+      if (status === kakao.maps.services.Status.OK) {
+        setAddSuccess(true);
         const markerPosition = new kakao.maps.LatLng(result[0].y, result[0].x);
+        setGps({
+          latitude: result[0].y,
+          longitude: result[0].x,
+        });
 
         const marker = new kakao.maps.Marker({
           position: markerPosition,
         });
-        const container = document.getElementById("ImgMap"), // 이미지 지도를 표시할 div
-          staticMapOption = {
-            center: new kakao.maps.LatLng(result[0].y, result[0].x), // 이미지 지도의 중심좌표
-            level: 5, // 이미지 지도의 확대 레벨
-            marker: marker, // 이미지 지도에 표시할 마커
-          };
-        const map = new kakao.maps.StaticMap(container, staticMapOption);
-      }
-    }
-  });
 
-  return <div id="ImgMap" style={{ width: "100%", height: "100%" }} />;
+        const options = {
+          center: new kakao.maps.LatLng(result[0].y, result[0].x), // 이미지 지도의 중심좌표
+          level: 5, // 이미지 지도의 확대 레벨
+          marker: marker, // 이미지 지도에 표시할 마커
+        };
+        const map = new kakao.maps.StaticMap(container, options);
+      } else {
+        setOpenSeachToast(true);
+        setSearchBtnClick(false);
+        setAddSuccess(false);
+      }
+    });
+  }
+  useEffect(() => {
+    if (imgMap.current) {
+      imgMap.current.innerHTML = "";
+      setMap();
+    }
+  }, [address]);
+
+  return (
+    <>
+      <div id="ImgMap" ref={imgMap} style={{ width: "100%", height: "100%" }} />
+    </>
+  );
 }
 
 export default ImageMap;
--- FE/src/features/schedule/create/buttons/PlaceAddModal.tsx
+++ FE/src/features/schedule/create/buttons/PlaceAddModal.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Modal from "components/Modal";
 import Button from "components/Button";
 import Toast from "components/Toast";
 import searchStyles from "../search/Search.module.css";
 import { AccountBalance, Hotel } from "@mui/icons-material";
-import { Box, Stack, TextField, Typography } from "@mui/material";
+import { Stack, TextField, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import ImageMap from "./ImageMap";
 
@@ -32,41 +32,65 @@ const CssTextField = styled(TextField)({
 
 function ButtonsAddModal(props: ButtonsAddModalType) {
   const { setAddPlaceModal } = props;
+  const addressInfo = useRef<HTMLInputElement>(null);
+  // const placeNameInfo = useRef<HTMLInputElement>(null);
   const [addCurrentTab, setAddCurrentTab] = useState("장소");
-  const [openToast, setOpenToast] = useState(false);
+  const [openAddToast, setOpenAddToast] = useState(false);
+  const [openSeachToast, setOpenSeachToast] = useState(false);
+  const [openTitleToast, setOpenTitleToast] = useState(false);
   const [addressValue, setAddressValue] = useState("");
-  const [addressSearch, setAddressSearch] = useState(false);
+  const [placeTitleValue, setPlaceTitleValue] = useState("");
+  const [searchBtnClick, setSearchBtnClick] = useState(false);
   const [addSuccess, setAddSuccess] = useState(false);
+  const [gps, setGps] = useState({
+    latitude: 33.450701,
+    longitude: 126.570667,
+  });
 
   const clickRadioButton = (e: React.ChangeEvent<HTMLInputElement>) => {
     setAddCurrentTab(e.target.value);
   };
 
   const addPlaceBtnClick = () => {
-    if (!addSuccess) setOpenToast(true);
+    if (!addSuccess) setOpenAddToast(true);
+    else if (placeTitleValue.length == 0) setOpenTitleToast(true);
     else {
-      setAddSuccess(true);
       // 장소추가 API
+      const data = {
+        location_name: placeTitleValue,
+        address: addressValue,
+        latitude: gps.latitude,
+        longitude: gps.longitude,
+        location_type: addCurrentTab,
+      };
     }
   };
 
   const searchClick = () => {
     // 장소 검색
-    if (addressValue.length == 0) return;
-    console.log(addressSearch);
+    if (addressInfo.current) {
+      setSearchBtnClick(true);
+      setAddressValue(addressInfo.current.value);
+    }
   };
 
   return (
     <>
       <Modal title="" modalClose={() => setAddPlaceModal(false)}>
-        <Stack alignItems="center" spacing={2}>
+        <Stack alignItems="center">
           <Typography variant="h5" display="block">
             장소 등록
           </Typography>
 
-          <Stack component="span" width="100%" height="15vh">
-            {addressSearch ? (
-              <ImageMap address={addressValue} setAddressSearch={setAddressSearch} />
+          <Stack component="span" width="100%" height="18vh" justifyContent="center" my={1}>
+            {searchBtnClick ? (
+              <ImageMap
+                address={addressValue}
+                setSearchBtnClick={setSearchBtnClick}
+                setOpenSeachToast={setOpenSeachToast}
+                setAddSuccess={setAddSuccess}
+                setGps={setGps}
+              />
             ) : (
               <>
                 <p style={{ color: "#999" }}>검색해도 나오지 않는 장소를 이곳에서 등록 후 다시 검색해보세요.</p>
@@ -103,31 +127,36 @@ function ButtonsAddModal(props: ButtonsAddModalType) {
               </label>
             </div>
 
-            <Stack direction="row" spacing={1} justifyContent="space-between">
-              <CssTextField
-                variant="filled"
-                label="주소 검색"
-                fullWidth
-                value={addressValue}
-                onChange={(e) => setAddressValue(e.target.value)}
-              />
-              <Button text="검색" color="pink" width="10%" onClick={() => searchClick()} />
-            </Stack>
+            <Stack spacing={1} pb={2}>
+              <Stack direction="row" spacing={1} justifyContent="space-between">
+                <CssTextField inputRef={addressInfo} variant="filled" label="주소 검색" fullWidth />
+                <Button text="검색" color="pink" width="10%" onClick={() => searchClick()} />
+              </Stack>
 
-            <CssTextField variant="filled" label="장소이름(최대 50자)" />
-            <CssTextField variant="filled" multiline label="설명사항(최대 200자, 생략 가능)" />
+              <CssTextField
+                value={placeTitleValue}
+                variant="filled"
+                label="장소이름(최대 20자)"
+                onChange={(e) => {
+                  setPlaceTitleValue(e.target.value);
+                }}
+              />
+              <CssTextField variant="filled" multiline label="설명사항(최대 100자, 생략 가능)" />
+            </Stack>
           </Stack>
 
           <Button
             text="장소추가"
-            color={addSuccess ? "main" : "gray"}
+            color={addSuccess && placeTitleValue.length > 0 ? "main" : "gray"}
             width="20%"
             radius
             onClick={() => addPlaceBtnClick()}
           />
         </Stack>
       </Modal>
-      <Toast message="검색된 주소만 등록할 수 있습니다. " open={openToast} onClose={setOpenToast} />
+      <Toast message="검색된 주소만 등록할 수 있습니다." open={openAddToast} onClose={setOpenAddToast} />
+      <Toast message="입력한 주소가 검색되지 않습니다." open={openSeachToast} onClose={setOpenSeachToast} />
+      <Toast message="장소이름을 입력해주세요." open={openTitleToast} onClose={setOpenTitleToast} />
     </>
   );
 }
