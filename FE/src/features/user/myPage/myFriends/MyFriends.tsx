5c659896b12863be745537834987697b754f2b8d
김주연 <jyeon3930@naver.com>
2023-03-16 15:15:37 +0900

    [FE] feat: 친구 목록 확인 API 연결

--- FE/src/features/user/myPage/myFriends/MyFriends.tsx
+++ FE/src/features/user/myPage/myFriends/MyFriends.tsx
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MyFriendsList from "./MyFriendsList";
 import styles from "./MyFriends.module.css";
 import Text from "components/Text";
 import Modal from "components/Modal";
 import MyFriendsAdd from "./MyFriendsAdd";
+import Axios from "api/JsonAxios";
+import api from "api/Api";
+import { useAppDispatch } from "app/hooks";
+import { setFriends } from "slices/friendSlice";
 
 const MyFriends = () => {
+  const dispatch = useAppDispatch();
+
   const [openAddFriendModal, setOpenAddFriendModal] = useState(false);
 
   const showAddFriendModal = () => {
     setOpenAddFriendModal(true);
   };
+
+  useEffect(() => {
+    Axios.get(api.friend.friend())
+      .then((res: any) => {
+        console.log(res);
+        dispatch(setFriends(res.data.data.friends));
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div className={styles.myFriends}>
--- FE/src/features/user/myPage/myFriends/MyFriendsList.tsx
+++ FE/src/features/user/myPage/myFriends/MyFriendsList.tsx
@@ -3,35 +3,43 @@ import styles from "./MyFriends.module.css";
 import sampleImg from "asset/sample/cat.png";
 import Text from "components/Text";
 import Button from "components/Button";
+import { useAppSelector } from "app/hooks";
+import { selectFriends } from "slices/friendSlice";
 
 const MyFriendsList = () => {
+  const friends = useAppSelector(selectFriends);
+
   return (
-    <div className={styles.myFriendsList}>
-      <div className={styles.friendInfo}>
-        <img className={styles.profileImg} src={sampleImg} alt="" />
-      </div>
+    <>
+      {friends.map((value, key) => (
+        <div className={styles.myFriendsList} key={key}>
+          <div className={styles.friendInfo}>
+            <img className={styles.profileImg} src={value.profile} alt="" />
+          </div>
 
-      <div className={styles.friendInfo}>
-        <Text value="이메일" color="main" bold />
-        <div style={{ margin: "1rem" }}></div>
-        <Text value="myro@myro.com" />
-      </div>
+          <div className={styles.friendInfo}>
+            <Text value="이메일" color="main" bold />
+            <div style={{ margin: "1rem" }}></div>
+            <Text value={value.email} />
+          </div>
 
-      <div className={styles.friendInfo}>
-        <Text value="닉네임" color="main" bold />
-        <div style={{ margin: "1rem" }}></div>
-        <Text value="마이로" />
-      </div>
+          <div className={styles.friendInfo}>
+            <Text value="닉네임" color="main" bold />
+            <div style={{ margin: "1rem" }}></div>
+            <Text value={value.nickname} />
+          </div>
 
-      <div className={styles.friendInfo}>
-        <Text value="함께 공유한 여행 일정" color="main" bold />
-        <div style={{ margin: "1rem" }}></div>
-        <Text value="2" />
-      </div>
-      <div className={styles.friendInfo}>
-        <Button text="친구 삭제" />
-      </div>
-    </div>
+          <div className={styles.friendInfo}>
+            {/* <Text value="함께 공유한 여행 일정" color="main" bold />
+            <div style={{ margin: "1rem" }}></div>
+            <Text value="2" /> */}
+          </div>
+          <div className={styles.friendInfo}>
+            <Button text="친구 삭제" />
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
 
