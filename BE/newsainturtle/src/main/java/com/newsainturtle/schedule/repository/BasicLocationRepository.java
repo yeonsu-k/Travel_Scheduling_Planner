a11347fcefbe0c008dde04304ec4224707fe5f89
tgb02087 <tgb02087@naver.com>
2023-03-21 02:28:23 +0900

    [BE] feat: 기본 장소 조회 쿼리 추가

--- BE/newsainturtle/src/main/java/com/newsainturtle/schedule/repository/BasicLocationRepository.java
+++ BE/newsainturtle/src/main/java/com/newsainturtle/schedule/repository/BasicLocationRepository.java
@@ -2,6 +2,13 @@ package com.newsainturtle.schedule.repository;
 
 import com.newsainturtle.schedule.entity.BasicLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BasicLocationRepository extends JpaRepository<BasicLocation,Long> {
+
+    @Query(value = "SELECT c FROM BasicLocation c WHERE c.regionId = :regionId and c.isHotel = :isHotel")
+    List<BasicLocation> findAllByRegionIdAndIsHotel(@Param("regionId") Long RegionId, @Param("isHotel") boolean isHotel);
 }
