de0f77a447b8edde16f3f3824bd629e8cd2992d4
tgb02087 <tgb02087@naver.com>
2023-03-20 23:07:10 +0900

    [BE] delete: Location of 메서드 삭제

--- BE/newsainturtle/src/main/java/com/newsainturtle/schedule/dto/LocationResponse.java
+++ BE/newsainturtle/src/main/java/com/newsainturtle/schedule/dto/LocationResponse.java
@@ -1,6 +1,5 @@
 package com.newsainturtle.schedule.dto;
 
-import com.newsainturtle.schedule.entity.Location;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -16,17 +15,4 @@ public class LocationResponse {
     private double longitude;
     private double latitude;
     private boolean isHotel;
-
-    public static LocationResponse of(Location location){
-        return LocationResponse.builder()
-                .locationId(location.getLocationId())
-                .regionId(location.getRegion().getRegionId())
-                .locationName(location.getLocationName())
-                .address(location.getAddress())
-                .profile("")
-                .longitude(location.getLongitude())
-                .latitude(location.getLatitude())
-                .isHotel(location.isHotel())
-                .build();
-    }
 }
