ba2565ee896add791f5b8b613a7a1ea2f15d9916
tgb02087 <tgb02087@naver.com>
2023-03-21 02:30:22 +0900

    [BE] delete: 칼럼 삭제

--- BE/newsainturtle/src/main/java/com/newsainturtle/schedule/dto/LocationResponse.java
+++ BE/newsainturtle/src/main/java/com/newsainturtle/schedule/dto/LocationResponse.java
@@ -1,17 +1,18 @@
 package com.newsainturtle.schedule.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class LocationResponse {
 
     private Long locationId;
     private Long regionId;
     private String locationName;
     private String address;
-    private String profile;
     private double longitude;
     private double latitude;
     private boolean isHotel;
