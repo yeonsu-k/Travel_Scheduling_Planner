cf073fcde2f8e730802609a2614b67caf6afef5f
tgb02087 <tgb02087@naver.com>
2023-03-20 23:11:25 +0900

    [BE] feat: BasicLocationRequest Dto 추가

---
+++ BE/newsainturtle/src/main/java/com/newsainturtle/schedule/dto/BasicLocationRequest.java
@@ -0,0 +1,21 @@
+package com.newsainturtle.schedule.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class BasicLocationRequest {
+
+    private Long regionId;
+
+    private String locationName;
+
+    private String address;
+
+    private Double longitude;
+
+    private Double latitude;
+
+    private boolean isHotel;
+}
