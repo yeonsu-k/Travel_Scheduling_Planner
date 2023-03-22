873dd66a98bfe25f2381089b8555899f6d64d7de
김주연 <jyeon3930@naver.com>
2023-03-23 05:10:33 +0900

    [FE] feat: 실시간 알림 기능 구현

--- FE/src/features/user/notice/Notice.tsx
+++ FE/src/features/user/notice/Notice.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./Notice.module.css";
 import Button from "components/Button";
 import NoticeItem from "./NoticeItem";
@@ -6,49 +6,81 @@ import { useAppDispatch, useAppSelector } from "app/hooks";
 import { resetNoticeList, selectNoticeList, setNoticeList } from "slices/noticeSlice";
 import Axios from "api/JsonAxios";
 import api from "api/Api";
-import SockJS from "sockjs-client";
-import StompJs from "@stomp/stompjs";
+import { selectUserInfo } from "slices/authSlice";
 
 const Notice = () => {
   const dispatch = useAppDispatch();
 
-  const accessToken = sessionStorage.getItem("accessToken");
+  const email = useAppSelector(selectUserInfo).email;
   const noticeList = useAppSelector(selectNoticeList);
 
-  if (accessToken) {
-    const webSocket = new WebSocket("ws://127.0.0.1:8080/ws-stomp");
+  const fireNotification = (title: string, options: any) => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
+    }
+  };
 
-    webSocket.onopen = () => {
-      console.log("연결 성공");
+  const webSocketUrl = process.env.REACT_APP_SOCKET_URL + email;
+
+  const ws = useRef<WebSocket | null>(null);
+  if (!ws.current) {
+    ws.current = new WebSocket(webSocketUrl);
+    ws.current.onopen = () => {
+      console.log("connected to " + webSocketUrl);
     };
-    webSocket.onerror = () => {
-      console.log("err");
+    ws.current.onmessage = (event) => {
+      console.log("메세지 옴: " + event.data);
+
+      console.log("data: ", event);
+
+      if (event.data.type === "friend") {
+        fireNotification("MYRO", {
+          body: `${event.data.senderNickname}님이 ${event.data.content}을 보냈습니다.`,
+        });
+      } else if (event.data.type === "schedule") {
+        fireNotification("MYRO", {
+          body: `${event.data.senderNickname}님이 일정공유 요청을 보냈습니다.`,
+        });
+      } else {
+        fireNotification("MYRO", {
+          body: event.data,
+        });
+      }
+    };
+    ws.current.onclose = (event) => {
+      console.log("disconnect from " + webSocketUrl);
+      console.log(event);
+    };
+    ws.current.onerror = (error) => {
+      console.log("connection error " + webSocketUrl);
+      console.log(error);
     };
   }
 
-  // const client = new StompJs.Client({
-  //   brokerURL: "ws://localhost:8080/ws-stomp",
-  //   connectHeaders: {
-  //     Authorization: `Bearer ${accessToken}`,
-  //   },
-  // });
-
   const onClickClearBtn = () => {
     Axios.delete(api.notification.notification())
-      .then((res: any) => {
+      .then((res) => {
         console.log(res);
         dispatch(resetNoticeList());
       })
-      .catch((err: any) => console.log(err));
+      .catch((err) => console.log(err));
   };
 
   useEffect(() => {
     Axios.get(api.notification.notification())
-      .then((res: any) => {
+      .then((res) => {
         console.log(res);
         dispatch(setNoticeList([...res.data.data.notifications]));
       })
-      .catch((err: any) => {
+      .catch((err) => {
         console.log(err);
       });
   }, []);
