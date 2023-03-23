49f4d61897fe6766090b21274f055e62b9fab59b
yunaghgh <yunaghgh@naver.com>
2023-03-23 22:14:03 +0900

    [BE] feat: 카카오 로그인 서비스 코드 작성

--- BE/newsainturtle/src/main/java/com/newsainturtle/auth/service/AuthService.java
+++ BE/newsainturtle/src/main/java/com/newsainturtle/auth/service/AuthService.java
@@ -4,7 +4,7 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import com.newsainturtle.auth.dto.*;
 import com.newsainturtle.auth.dto.kakao.KakaoInfoMapResponse;
 import com.newsainturtle.auth.dto.kakao.KakaoInfoResponse;
-import com.newsainturtle.auth.dto.KakaoLoginCodeUrlResponse;
+import com.newsainturtle.auth.dto.KakaoCodeUrlResponse;
 import com.newsainturtle.auth.dto.kakao.KakaoTokenResponse;
 import com.newsainturtle.auth.exception.InvalidTokenException;
 import com.newsainturtle.auth.exception.KakaoLoginException;
@@ -24,7 +24,6 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 import static com.newsainturtle.auth.constant.AuthConstant.KAKAO_API_URL;
 import static com.newsainturtle.auth.constant.AuthConstant.KAKAO_AUTH_URL;
@@ -72,7 +71,7 @@ public class AuthService {
     }
 
     @Transactional(readOnly = true)
-    public KakaoLoginCodeUrlResponse getKakaoAuthUrl() {
+    public KakaoCodeUrlResponse getKakaoAuthUrl() {
         StringBuilder sb = new StringBuilder();
         sb.append(KAKAO_AUTH_URL)
                 .append("/oauth/authorize?")
@@ -82,11 +81,107 @@ public class AuthService {
                 .append("redirect_uri=")
                 .append(redirectURL)
                 .append("&response_type=code");
-        KakaoLoginCodeUrlResponse kakaoLoginCodeURLResponse = KakaoLoginCodeUrlResponse.builder()
+        KakaoCodeUrlResponse kakaoCodeURLResponse = KakaoCodeUrlResponse.builder()
                 .url(sb.toString())
                 .build();
-        return kakaoLoginCodeURLResponse;
+        return kakaoCodeURLResponse;
     }
 
+    public LoginResponse loginKakao(String code) {
+        String accessToken = getKakaoToken(code);
+        return getKakaoInfo(accessToken);
+    }
+
+    private String getKakaoToken(String code){
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("grant_type", "authorization_code");
+        formData.add("client_id", kakaoClientID);
+        formData.add("redirect_uri", redirectURL);
+        formData.add("client_secret", kakaoClientSecretCode);
+        formData.add("code", code);
+
+        KakaoTokenResponse kakaoTokenResponse =
+                WebClient.create(KAKAO_AUTH_URL).post()
+                        .uri("/oauth/token")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .body(BodyInserters.fromFormData(formData))
+                        .retrieve()
+                        .onStatus(status -> status.is4xxClientError()
+                                        || status.is5xxServerError()
+                                , clientResponse ->
+                                        clientResponse.bodyToMono(String.class)
+                                                .map(body -> new RuntimeException(body)))
+                        .bodyToMono(KakaoTokenResponse.class)
+                        .block();
+        return kakaoTokenResponse.getAccessToken();
+    }
+
+    private KakaoInfoResponse connectKakaoUser(String accessToken){
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("property_keys", "[\"kakao_account.profile\",\"kakao_account.email\"]");
+
+        KakaoInfoMapResponse kakaoInfoMapResponse =
+                WebClient.create(KAKAO_API_URL).post()
+                        .uri("/v2/user/me")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .body(BodyInserters.fromFormData(formData))
+                        .retrieve()
+                        .onStatus(status -> status.is4xxClientError()
+                                        || status.is5xxServerError()
+                                , clientResponse ->
+                                        clientResponse.bodyToMono(String.class)
+                                                .map(body -> new RuntimeException(body)))
+                        .bodyToMono(KakaoInfoMapResponse.class)
+                        .block();
+
+        return kakaoInfoMapResponse.getKakaoAccount();
+    }
+
+    private void disconnectKakaoUser(String accessToken){
+        WebClient.create(KAKAO_API_URL).post()
+                .uri("/v1/user/unlink")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .retrieve()
+                .onStatus(status -> status.is4xxClientError()
+                                || status.is5xxServerError()
+                        , clientResponse ->
+                                clientResponse.bodyToMono(String.class)
+                                        .map(body -> new RuntimeException(body)))
+                .bodyToMono(String.class).block();
+    }
+
+    private LoginResponse getKakaoInfo(String accessToken) {
+
+        KakaoInfoResponse kakaoInfoResponse = connectKakaoUser(accessToken);
+
+        if (kakaoInfoResponse != null && kakaoInfoResponse.isHasEmail() && !kakaoInfoResponse.isEmailNeedsAgreement() && !kakaoInfoResponse.isProfileNicknameNeedsAgreement()) {
+            if (kakaoInfoResponse.isEmailValid() && kakaoInfoResponse.getEmail() != null) {
+                User user = userRepository.findByEmail(kakaoInfoResponse.getEmail());
+                if (user == null) {
+                    if (kakaoInfoResponse.getProfile() != null && kakaoInfoResponse.getProfile().getNickname() != null) {
+                        final User newUser = User.builder()
+                                .email(kakaoInfoResponse.getEmail())
+                                .nickname(kakaoInfoResponse.getProfile().getNickname())
+                                .profile("")
+                                .withdraw(false)
+                                .kakao(true)
+                                .build();
+                        userRepository.save(newUser);
+                        return LoginResponse.builder().accessToken(jwtTokenProvider.createAccessToken(newUser.getEmail())).nickname(newUser.getNickname()).profile(newUser.getProfile()).build();
+                    }
+                } else if (!user.isKakao() || user.isWithdraw()) {
+                    disconnectKakaoUser(accessToken);
+                    throw new UnavailableEmailException();
+                } else {
+                    return LoginResponse.builder().accessToken(jwtTokenProvider.createAccessToken(user.getEmail())).nickname(user.getNickname()).profile(user.getProfile()).build();
+                }
+            }
+        }
+
+        disconnectKakaoUser(accessToken);
+        throw new KakaoLoginException();
+    }
 
 }
