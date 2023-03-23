5d0731536f0225c87c1e001978b0c37180a6dc26
SungSoo <tjdtn6725@naver.com>
2023-03-23 16:18:17 +0900

    [BE] feat: UserService.java 회원 탈퇴 서비스 작성

--- BE/newsainturtle/src/main/java/com/newsainturtle/user/service/UserService.java
+++ BE/newsainturtle/src/main/java/com/newsainturtle/user/service/UserService.java
@@ -5,6 +5,8 @@ import com.newsainturtle.auth.dto.EmailDuplicateCheckResponse;
 import com.newsainturtle.auth.dto.UserJoinRequest;
 import com.newsainturtle.auth.dto.UserJoinResponse;
 import com.newsainturtle.auth.exception.NoEmailCheckException;
+import com.newsainturtle.friend.repository.FriendRepository;
+import com.newsainturtle.notification.repository.NotificationRepository;
 import com.newsainturtle.schedule.entity.Schedule;
 import com.newsainturtle.schedule.entity.ScheduleMember;
 import com.newsainturtle.schedule.repository.ScheduleMemberRepository;
@@ -33,6 +35,8 @@ public class UserService {
     private final UserRepository userRepository;
     private final ScheduleRepository scheduleRepository;
     private final ScheduleMemberRepository scheduleMemberRepository;
+    private final FriendRepository friendRepository;
+    private final NotificationRepository notificationRepository;
 
     public EmailDuplicateCheckResponse emailDuplicateCheck(EmailDuplicateCheckRequest emailDuplicateCheckRequest) {
         User user = userRepository.findByEmail(emailDuplicateCheckRequest.getEmail());
@@ -149,5 +153,13 @@ public class UserService {
                 scheduleMemberRepository.deleteByScheduleIdAndUserEmail(schedule_id, email);
             }
         }
+    }
+
+    @Transactional
+    public void withdrawUser(String email) {
+        User user = userRepository.findByEmail(email);
+        user.withDrawUser();
+        friendRepository.deleteAllByRequestUserOrReceiveUser(user, user);
+        notificationRepository.deleteByReceiveUser(user);
     }
 }
