d069bac3009d79256dc397fda081283c298a5b83
jhkang9820 <jhkang9820@gmail.com>
2023-03-28 01:20:14 +0900

    [FE] feat: 마이페이지 일정 목록 조회 API 연결 및 CSS 수정

--- FE/src/features/user/myPage/mySchedule/MyScheduleList.tsx
+++ FE/src/features/user/myPage/mySchedule/MyScheduleList.tsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MyScheduleListItem from "./MyScheduleListItem";
+import styles from "./MySchedule.module.css";
+import Axios from "api/JsonAxios";
+import api from "api/Api";
+
+export interface MyScheduleConfig {
+  schedule_id: number;
+  host: string;
+  region_name: string;
+  schedule_name: string;
+  start_day: string;
+  end_day: string;
+  mine: boolean;
+  private: boolean;
+  modifiedTime: string;
+}
 
 const MyScheduleList = () => {
+  const [mySchedule, setMySchedule] = useState<MyScheduleConfig[]>([]);
+
+  const getMySchedule = async () => {
+    await Axios.get(api.user.getScheduleList())
+      .then((res) => {
+        setMySchedule(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getMySchedule();
+  }, []);
+
   return (
-    <div>
-      <MyScheduleListItem />
-      <MyScheduleListItem />
-      <MyScheduleListItem />
+    <div className={styles.myScheduleContainer}>
+      {mySchedule.map((item: MyScheduleConfig, i: number) => (
+        <MyScheduleListItem key={i} {...item} />
+      ))}
     </div>
   );
 };
