22cc374f44771dad64cb5aee7d3e74de7b5fd80e
김주연 <jyeon3930@naver.com>
2023-03-30 16:42:13 +0900

    [FE] feat: 일정 수정 페이지 내 장소 검색 및 등록 기능 추가

---
+++ FE/src/features/schedule/edit/search/SearchList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface SearchListProps {
+  select: string;
+  keyword: string;
+  searchView: boolean;
+}
+
+const SearchList = () => {
+  return <div></div>;
+};
+
+export default SearchList;
