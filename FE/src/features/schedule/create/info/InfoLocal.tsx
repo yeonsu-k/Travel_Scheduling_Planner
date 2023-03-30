a315e7c723ea69b518006e416ed19f8a6f81a36b
dustnzlzl <dustnzlzl@naver.com>
2023-03-30 18:13:04 +0900

    [FE] feat: 일정페이지에서 장소선택 기능 추가

--- FE/src/features/schedule/create/info/InfoLocal.tsx
+++ FE/src/features/schedule/create/info/InfoLocal.tsx
@@ -1,47 +1,24 @@
 import React from "react";
-import { selectRegion, setRegion } from "slices/scheduleCreateSlice";
-import { useAppDispatch, useAppSelector } from "app/hooks";
+import { selectRegion } from "slices/scheduleCreateSlice";
+import { useAppSelector } from "app/hooks";
 import styles from "./Info.module.css";
 import Modal from "components/Modal";
-import Button from "components/Button";
-import Input from "components/Input";
 import { Stack } from "@mui/system";
+import InfoLocalSelect from "./InfoLocalSelect";
 
 function InfoLocal() {
-  const dispatch = useAppDispatch();
   const region = useAppSelector(selectRegion);
   const [ModalOpen, setModalOpen] = React.useState(false);
-  const [searchLocal, setSearchLocal] = React.useState({
-    id: 1,
-    name: "서울",
-  });
 
-  const showModal = () => {
-    setModalOpen(true);
-  };
-
-  const choicePlace = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.value) return;
-    setSearchLocal({
-      id: 1, // 서울만 가능 추후 regionId로 수정
-      name: e.target.value,
-    });
-  };
-
-  const savePlace = () => {
-    dispatch(setRegion(searchLocal));
-    setModalOpen(false);
-  };
   return (
     <div className={styles.content}>
-      <Stack alignItems="center" onClick={showModal} sx={{ cursor: "pointer" }}>
+      <Stack alignItems="center" onClick={() => setModalOpen(true)} sx={{ cursor: "pointer" }}>
         <span className={styles.localText}>{region.name}</span>
         <span className={styles.localText_en}>Busan</span>
       </Stack>
       {ModalOpen && (
         <Modal title="지역선택" modalClose={() => setModalOpen(false)}>
-          <Input placeholder="주소를 입력해주세요" onChange={(e) => choicePlace(e)} />
-          <Button text="확인" color="main" radius onClick={savePlace} />
+          <InfoLocalSelect modalClose={() => setModalOpen(false)} />
         </Modal>
       )}
     </div>
