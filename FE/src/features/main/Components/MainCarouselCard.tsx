622703b4680c237123c9df1817ffca0f19936abd
jhkang9820 <jhkang9820@gmail.com>
2023-03-30 14:50:17 +0900

    [FE] design: 여행지 캐러셀 카드 디자인 변경

--- FE/src/features/main/Components/MainCarouselCard.tsx
+++ FE/src/features/main/Components/MainCarouselCard.tsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { DestinationConfig } from "slices/mainSlice";
 import styles from "../Main.module.css";
 
 interface CarouselCardProps {
-  src: string;
+  type: string;
+  item?: DestinationConfig;
 }
 
-const MainCarouselCard = ({ src }: CarouselCardProps) => {
+const MainCarouselCard = ({ type, item }: CarouselCardProps) => {
   return (
-    <div className={styles.carouselCard}>
-      <img className={styles.carouselImg} src={src} />
+    <div className={styles.carouselCardContainer}>
+      <div className={styles.carouselCard}>
+        <img src={item?.regionImageURL} />
+      </div>
+      <div className={styles.carouselText}>
+        <p>{item?.englishName}</p>
+        <span>대한민국 {item?.regionName}</span>
+      </div>
     </div>
   );
 };
