d0862dede1a918f3378ca32a40063a25d22b574d
jhkang9820 <jhkang9820@gmail.com>
2023-03-30 21:01:50 +0900

    [FE] design: 여행기 모달창 CSS 수정

--- FE/src/features/main/MainTravelLog/MainTravelLogModal.tsx
+++ FE/src/features/main/MainTravelLog/MainTravelLogModal.tsx
@@ -1,4 +1,5 @@
 import Modal from "@mui/material/Modal";
+import Text from "components/Text";
 import React, { Dispatch, SetStateAction } from "react";
 import styles from "../Main.module.css";
 import MainTravelLogItem from "./MainTravelLogItem";
@@ -12,7 +13,22 @@ const MainTravelLogModal = ({ open, setModalOpen }: ModalTravelLogModalProps) =>
   return (
     <Modal open={open} onClose={() => setModalOpen(false)}>
       <div className={styles.logModalContainer}>
-        <MainTravelLogItem />
+        <div className={styles.logModalTitle}>
+          <Text value="장소 목록" type="groupTitle" />
+        </div>
+        <div className={styles.logModalInfoCont}>
+          {/* <div className={styles.logModalInfo}>
+            <div className={styles.logInfoImg}>
+              <img src="https://www.myro.co.kr/myro_image/travelog/blog_001.jpg" />
+            </div>
+          </div> */}
+          <div className={styles.logModalItemCont}>
+            <MainTravelLogItem />
+            <MainTravelLogItem />
+            <MainTravelLogItem />
+            <MainTravelLogItem />
+          </div>
+        </div>
       </div>
     </Modal>
   );
