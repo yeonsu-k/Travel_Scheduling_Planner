75e6d6b7efa8b28a6f619583f627e32cc544d32a
jhkang9820 <jhkang9820@gmail.com>
2023-03-31 17:36:23 +0900

    [FE] feat: 인기 여행지 모달창 연결

--- FE/src/features/main/Components/MainCarouselCard.tsx
+++ FE/src/features/main/Components/MainCarouselCard.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { forwardRef, useEffect, useState } from "react";
 import { DestinationConfig } from "slices/mainSlice";
 import styles from "../Main.module.css";
 import MainTravelLog, { LogConfig } from "../MainTravelLog/MainTravelLog";
 import MainTravelLogModal from "../MainTravelLog/MainTravelLogModal";
+import { Modal } from "@mui/material";
+import MainDestinationModal from "../MainDestinations/MainDestinationModal";
 
 interface CarouselCardProps {
   type: string;
@@ -11,7 +13,15 @@ interface CarouselCardProps {
 }
 
 const MainCarouselCard = ({ type, item, info }: CarouselCardProps) => {
-  const [ModalOpen, setModalOpen] = useState(false);
+  const [ModalOpen, setModalOpen] = useState<boolean>(false);
+  const [destiModalOpen, setDestiModalOpen] = useState<boolean>(false);
+
+  const Bar = forwardRef((props: any, ref: any) => (
+    <span {...props} ref={ref}>
+      {props.children}
+    </span>
+  ));
+  Bar.displayName = "Bar";
 
   return (
     <>
@@ -27,7 +37,7 @@ const MainCarouselCard = ({ type, item, info }: CarouselCardProps) => {
             </div>
           </div>
         ) : (
-          <div>
+          <div style={{ cursor: "pointer" }} onClick={() => setDestiModalOpen(true)}>
             <div className={styles.carouselCard}>
               <img src={item?.regionImageURL} />
             </div>
@@ -39,6 +49,13 @@ const MainCarouselCard = ({ type, item, info }: CarouselCardProps) => {
         )}
       </div>
       {ModalOpen ? <MainTravelLogModal open={ModalOpen} setModalOpen={setModalOpen} /> : null}
+      {destiModalOpen && item ? (
+        <Modal open={destiModalOpen} onClose={() => setDestiModalOpen(false)}>
+          <Bar>
+            <MainDestinationModal item={item} setModalOpen={setDestiModalOpen} />
+          </Bar>
+        </Modal>
+      ) : null}
     </>
   );
 };
