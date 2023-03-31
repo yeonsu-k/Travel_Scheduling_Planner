72cb05650dfe4575f60c6f6002d4f07e750ae146
jhkang9820 <jhkang9820@gmail.com>
2023-04-01 04:09:27 +0900

    [FE] design: 여행기 여행지 이미지 기본값 변경

--- FE/src/features/main/MainTravelLog/MainTravelLogItem.tsx
+++ FE/src/features/main/MainTravelLog/MainTravelLogItem.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "../Main.module.css";
-import { locationConfig } from "slices/mainSlice";
 
 interface MainTravelLogItemProps {
   location: any;
@@ -10,7 +9,7 @@ const MainTravelLogItem = ({ location }: MainTravelLogItemProps) => {
   return (
     <div className={styles.logModalItem}>
       <div className={styles.logModalImgCont}>
-        <img src="https://www.myro.co.kr/myro_image/travelog/blog_001.jpg" />
+        <img src="https://www.myro.co.kr/getSpotImage/gyeongju?no=1110" />
       </div>
       <div className={styles.logModalTextCont}>
         <span>{location.locationName}</span>
