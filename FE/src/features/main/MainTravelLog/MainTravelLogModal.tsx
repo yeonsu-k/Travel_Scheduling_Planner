8dbb9d8a9269013c83698f4d5bdcfb384f809dcc
jhkang9820 <jhkang9820@gmail.com>
2023-04-01 04:08:48 +0900

    [FE] design: 여행기 모달 CSS 수정

--- FE/src/features/main/MainTravelLog/MainTravelLogModal.tsx
+++ FE/src/features/main/MainTravelLog/MainTravelLogModal.tsx
@@ -16,16 +16,25 @@ const MainTravelLogModal = ({ open, setModalOpen, travelLog }: ModalTravelLogMod
     <Modal open={open} onClose={() => setModalOpen(false)}>
       <div className={styles.logModalContainer}>
         <div className={styles.logModalTitle}>
-          <Text value="장소 목록" type="groupTitle" />
+          <Text value="여행기" type="groupTitle" />
+          <span>TRAVELLOG</span>
         </div>
         <div className={styles.logModalInfoCont}>
           <div className={styles.logInfoImg}>
             <img src={travelLog?.logImg} />
+            <span>{travelLog?.scheduleName}</span>
+            <span>{travelLog?.hostEmail}</span>
           </div>
+
           <div className={styles.logModalItemCont}>
-            {travelLog?.scheduleLocations.map((location: locationConfig, i: number) => (
-              <MainTravelLogItem key={i} location={location} />
-            ))}
+            <div className={styles.logModalItemTitle}>
+              <Text value="장소 목록" type="text" />
+            </div>
+            <div className={styles.logModalLogItem}>
+              {travelLog?.scheduleLocations.map((location: locationConfig, i: number) => (
+                <MainTravelLogItem key={i} location={location} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
