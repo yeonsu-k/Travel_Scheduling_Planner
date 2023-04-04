4bb634fde0c184065b775d2c313975a8e9b6ef1d
tgb02087 <tgb02087@naver.com>
2023-04-04 18:32:29 +0900

    [BE] feat: LocatioNSet Request Dto 추가

---
+++ BE/newsainturtle/src/main/java/com/newsainturtle/schedule/dto/LocationSetRequest.java
@@ -0,0 +1,25 @@
+package com.newsainturtle.schedule.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LocationSetRequest {
+
+    private Long lcationId;
+
+    private Long regionId;
+
+    private String locationName;
+
+    private String address;
+
+    private double longtitude;
+
+    private double latitude;
+
+    private boolean isHotel;
+
+    private String time;
+}
