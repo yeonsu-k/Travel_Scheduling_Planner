4ff513e93c0a1859bea881db512c442006c37c8c
김주연 <jyeon3930@naver.com>
2023-04-04 23:24:49 +0900

    [FE] feat: 알림 토스트 메시지 추가

--- FE/src/features/user/notice/NoticeToast.tsx
+++ FE/src/features/user/notice/NoticeToast.tsx
@@ -1,34 +1,11 @@
 import React, { Dispatch, SetStateAction, useEffect } from "react";
-import { Snackbar, Zoom, styled } from "@mui/material";
+import { Alert, Snackbar, Zoom, styled } from "@mui/material";
 
 interface NoticeToastProps {
   message: string;
   open: boolean;
   onClose: Dispatch<SetStateAction<boolean>>;
 }
-
-const SnackbarStyle = styled(Snackbar)(() => ({
-  "&.MuiSnackbar-root": {
-    margin: "0",
-    width: "100%",
-    height: "100%",
-    // top: "50%",
-    // left: "50%",
-    // transform: "translate(-50%, -50%)",
-    overflow: "auto",
-    zIndex: "100",
-  },
-  ".MuiPaper-root": {
-    backgroundColor: "rgba(0, 0, 0, 0.8)",
-    justifyContent: "center",
-  },
-  ".MuiSnackbarContent-root": {
-    fontFamily: "Pretendard-Regular",
-    fontSize: "0.95rem",
-    transition: "none",
-    padding: "32px 64px",
-  },
-}));
 
 const NoticeToast = ({ message, open, onClose }: NoticeToastProps) => {
   const timer = setTimeout(() => onClose(false), 3000);
@@ -42,12 +19,11 @@ const NoticeToast = ({ message, open, onClose }: NoticeToastProps) => {
   return (
     <>
       {open && (
-        <SnackbarStyle
-          anchorOrigin={{ vertical: "top", horizontal: "right" }}
-          open={open}
-          TransitionComponent={Zoom}
-          message={message}
-        />
+        <Snackbar anchorOrigin={{ vertical: "top", horizontal: "right" }} open={open} TransitionComponent={Zoom}>
+          <Alert severity="info" sx={{ width: "100%" }}>
+            {message}
+          </Alert>
+        </Snackbar>
       )}
     </>
   );
--- FE/src/features/user/notice/Socket.ts
+++ FE/src/features/user/notice/Socket.ts
@@ -1,79 +1,9 @@
-import Axios from "api/JsonAxios";
-import { noticeListProps } from "./Notice";
-import api from "api/Api";
-import { setNotiNumber } from "slices/mainSlice";
-import { useAppDispatch } from "app/hooks";
-
 export let socket: WebSocket;
-
-const fireNotification = (title: string, options: object) => {
-  new Notification(title, options);
-};
 
 export const connectSocket = (email: string) => {
   socket = new WebSocket(process.env.REACT_APP_SOCKET_URL + email);
-
-  if (socket) {
-    socket.onopen = () => {
-      console.log("connected");
-    };
-    socket.onmessage = (event) => {
-      console.log("메세지 옴: " + event.data);
-
-      const data = JSON.parse(event.data);
-
-      console.log("data: ", data.type);
-
-      if (data.type === "friend") {
-        console.log("friend");
-        fireNotification("MYRO", {
-          body: `${data.senderNickname}님이 ${data.content}을 보냈습니다.`,
-        });
-      } else if (data.type === "schedule") {
-        fireNotification("MYRO", {
-          body: `${data.senderNickname}님이 ${data.content} 일정을 공유했습니다.`,
-        });
-      }
-
-      getNotification();
-    };
-    socket.onclose = (event) => {
-      console.log("disconnect");
-      console.log(event);
-    };
-    socket.onerror = (error) => {
-      console.log("connection error ");
-      console.log(error);
-    };
-  }
 };
 
 export const disconnectSocket = () => {
   socket.close();
-};
-
-let noticeList: noticeListProps[] = [];
-const getNotification = async () => {
-  const dispatch = useAppDispatch();
-
-  let notificationCount = 0;
-
-  await Axios.get(api.notification.notification())
-    .then((res) => {
-      console.log(res);
-      noticeList = [...res.data.data.notifications];
-      console.log("list", noticeList);
-
-      noticeList.map((value, key) => {
-        if (value.status === "NO_RESPONSE") {
-          notificationCount++;
-        }
-      });
-
-      console.log("cnt: ", notificationCount);
-      dispatch(setNotiNumber({ notiNumber: notificationCount }));
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 };
