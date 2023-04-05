018c505b6af9dc5b3e1e19ca3b1df725639ee0fa
김주연 <jyeon3930@naver.com>
2023-04-06 03:30:38 +0900

    [FE] remove: 이전 일정 저장 redux 삭제

--- FE/src/slices/scheduleEditSlice.ts
+++ FE/src/slices/scheduleEditSlice.ts
@@ -2,25 +2,6 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { rootState } from "app/store";
 import { basicConfig } from "slices/scheduleCreateSlice";
 
-export interface placeInfoConfig {
-  id: number;
-  image: string;
-  name: string;
-  address: string;
-  latitude: number;
-  longitude: number;
-  time: string;
-  startTime: string;
-  endTime: string;
-}
-
-export interface fullScheduleListConfig {
-  day: number;
-  startHour: number;
-  startMinute: number;
-  dayList: placeInfoConfig[];
-}
-
 export interface scheduleConfig {
   location: basicConfig;
   day: number;
@@ -30,23 +11,12 @@ export interface scheduleConfig {
 }
 
 interface scheduleEditConfig {
-  fullScheduleList: fullScheduleListConfig[];
-  keepPlaceList: placeInfoConfig[];
+  keepPlaceList: basicConfig[];
   scheduleList: scheduleConfig[][];
 }
 
 const initialState: scheduleEditConfig = {
-  fullScheduleList: [],
-  keepPlaceList: [
-    // {
-    //   id: 8,
-    //   img: "src",
-    //   placeName: "월정리 해수욕장",
-    //   time: "2시간 0분",
-    //   startTime: "10:00",
-    //   endTime: "12:00",
-    // },
-  ],
+  keepPlaceList: [],
   scheduleList: [],
 };
 
@@ -54,17 +24,14 @@ const scheduleEditSlice = createSlice({
   name: "scheduleEdit",
   initialState,
   reducers: {
-    setFullScheduleList: (state, { payload }) => {
-      state.fullScheduleList = payload;
-    },
     setKeepPlaceList: (state, { payload }) => {
       state.keepPlaceList = payload;
     },
     setStayTime: (state, { payload: { day, index, startTime, endTime } }) => {
-      state.fullScheduleList[day - 1].dayList[index].startTime = startTime;
-      state.fullScheduleList[day - 1].dayList[index].endTime = endTime;
-      console.log("startTime", state.fullScheduleList[day - 1].dayList[index].startTime);
-      console.log("endTime: ", state.fullScheduleList[day - 1].dayList[index].endTime);
+      state.scheduleList[day - 1][index].startTime = startTime;
+      state.scheduleList[day - 1][index].endTime = endTime;
+      console.log("startTime", state.scheduleList[day - 1][index].startTime);
+      console.log("endTime: ", state.scheduleList[day - 1][index].endTime);
     },
     setscheduleList: (state, action: PayloadAction<scheduleEditConfig["scheduleList"]>) => {
       state.scheduleList = action.payload;
@@ -72,9 +39,8 @@ const scheduleEditSlice = createSlice({
   },
 });
 
-export const { setFullScheduleList, setKeepPlaceList, setStayTime, setscheduleList } = scheduleEditSlice.actions;
+export const { setKeepPlaceList, setStayTime, setscheduleList } = scheduleEditSlice.actions;
 
-export const selectFullScheduleList = (state: rootState) => state.scheduleEdit.fullScheduleList;
 export const selectKeepPlaceList = (state: rootState) => state.scheduleEdit.keepPlaceList;
 export const selectScheduleList = (state: rootState) => state.scheduleEdit.scheduleList;
 
